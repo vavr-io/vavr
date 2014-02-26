cfbd823fcb68a6189a2cfcd8452fd5ace1d520c1
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-02-26 23:35:28 +0100

    added javadoc

--- src/main/java/javaslang/util/None.java
+++ src/main/java/javaslang/util/None.java
@@ -7,23 +7,33 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public final class None<T> implements Option<T> {
-	
+
 	private static final None<?> NONE = new None<>();
-	
+
 	private None() {
 	}
-	
+
+	/**
+	 * Returns the singleton instance of <code>None</code> as <code>None&lt;T&gt;</code> in the context of a type
+	 * &lt;T&gt;, e.g.
+	 * 
+	 * <pre>
+	 * <code>final Option&lt;Integer&gt; o = None.instance(); // o is of type None&lt;Integer&gt;</code>
+	 * </pre>
+	 * 
+	 * @return None
+	 */
 	public static <T> None<T> instance() {
-        @SuppressWarnings("unchecked")
+		@SuppressWarnings("unchecked")
 		final None<T> none = (None<T>) NONE;
-        return none;
+		return none;
 	}
-	
+
 	@Override
 	public T get() {
 		throw new NoSuchElementException("No value present");
 	}
-	
+
 	@Override
 	public T orElse(T other) {
 		return other;
@@ -38,20 +48,21 @@ public final class None<T> implements Option<T> {
 	public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X {
 		throw exceptionSupplier.get();
 	}
-	
+
 	@Override
 	public boolean isPresent() {
 		return false;
 	}
-	
+
 	@Override
 	public void ifPresent(Consumer<? super T> consumer) {
 		// nothing to do
 	}
-    
+
 	@Override
 	public Option<T> filter(Predicate<? super T> predicate) {
-		return None.instance(); // semantically correct but structurally the same that <code>return this;</code>
+		// semantically correct but structurally the same that <code>return this;</code>
+		return None.instance();
 	}
 
 	@Override
@@ -63,8 +74,8 @@ public final class None<T> implements Option<T> {
 	public <U> Option<U> flatMap(Function<? super T, Option<U>> mapper) {
 		return None.instance();
 	}
-	
-	// super.equals and super.hashCode are fine
+
+	// super.equals and super.hashCode are fine because this is a singleton
 
 	@Override
 	public String toString() {
