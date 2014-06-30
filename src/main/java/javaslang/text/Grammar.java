02fe3e5f3d0e15be208b0023dd4b79fb780994d1
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-06-30 10:36:15 +0200

    Made EOF a first class citizen.

--- src/main/java/javaslang/text/Grammar.java
+++ src/main/java/javaslang/text/Grammar.java
@@ -12,8 +12,6 @@ import java.util.Set;
 import java.util.function.Supplier;
 
 import javaslang.either.Either;
-import javaslang.either.Left;
-import javaslang.either.Right;
 import javaslang.exception.Failure;
 import javaslang.exception.Success;
 import javaslang.exception.Try;
@@ -23,7 +21,6 @@ import javaslang.exception.Try;
 //       - https://github.com/antlr/grammars-v4/blob/master/antlr4/ANTLRv4Parser.g4
 // TODO: Make regular expressions first class members of grammar definitions, e.g. fragment DIGITS: '1'..'9' '0'..'9'*;
 //		- '|' <space> * ? + .. \n \r \t etc. (escape reserved symbols, e.g. quote ')
-// TODO: Make EOF a first class citizen (and do not add it implicitly)
 // TODO: Make grammars compatible to Antlr4 grammars (i.e. parse (and stringify) them - https://github.com/antlr/grammars-v4/blob/master/java8/Java8.g4
 // TODO: Add fragments - http://stackoverflow.com/questions/6487593/what-does-fragment-means-in-antlr
 // TODO: CST to AST transformation (as external DSL within the grammar)
@@ -32,13 +29,11 @@ import javaslang.exception.Try;
 // TODO: remove Branch, Sequence and Multiplicity nodes if they have no name/id
 public class Grammar {
 	
-	static final EOF EOF = new EOF();
-
 	final Supplier<Parser> parser;
 
 	public Grammar(Supplier<Parser> parser) {
 		require(parser != null, "parser is null");
-		this.parser = new Sequence(parser, EOF);
+		this.parser = new Sequence(parser, EOF.instance());
 	}
 
 	public Try<Tree<Token>> parse(String text) {
@@ -57,32 +52,4 @@ public class Grammar {
 		return rule.append(definitions).toString();
 	}
 	
-	/**
-	 * End of file parser.
-	 */
-	static class EOF extends Parser {
-		
-		private static final String NAME = "<EOF>";
-		
-		@Override
-		public Either<Integer, Tree<Token>> parse(String text, int index) {
-			if (index == text.length()) {
-				return new Right<>(new Tree<>(NAME, new Token(text, index, index)));
-			} else {
-				return new Left<>(index);
-			}
-		}
-		
-		@Override
-		protected void stringify(StringBuilder rule, StringBuilder definitions, Set<String> visited) {
-			rule.append(NAME);
-		}
-
-		@Override
-		public String toString() {
-			return NAME;
-		}
-
-	}
-
 }
