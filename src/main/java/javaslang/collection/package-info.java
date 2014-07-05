e3d93eeb69ddd619bcb2436a29ef12798191872e
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-07-05 12:02:38 +0200

    Added TODO - mid term goal is to implement nice functional/immutable collections like List, Set, Map, etc.

--- src/main/java/javaslang/collection/package-info.java
+++ src/main/java/javaslang/collection/package-info.java
@@ -1,4 +1,5 @@
 /**
  * Provides extension methods for java.util's Collection, List and Set.
+ * TODO: mid term goal is to design a immutable collection library with mature, math. operations
  */
 package javaslang.collection;
