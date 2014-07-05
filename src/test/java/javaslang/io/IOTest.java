6796f90a3419e812ee1d41eefbe7c6effe3a5d41
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-07-05 20:10:44 +0200

    Added logo to test classes

--- src/test/java/javaslang/io/IOTest.java
+++ src/test/java/javaslang/io/IOTest.java
@@ -1,3 +1,8 @@
+/**    / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
 package javaslang.io;
 
 import static javaslang.io.IO.UTF8;
--- src/test/java/javaslang/text/ParserTest.java
+++ src/test/java/javaslang/text/ParserTest.java
@@ -1,3 +1,8 @@
+/**    / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
 package javaslang.text;
 
 import static javaslang.text.Multiplicity.Bounds.ZERO_TO_N;
