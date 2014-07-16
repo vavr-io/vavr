3843360adcff164d6f3b08a01897753de66f2a40
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-07-17 00:13:43 +0200

    Added tests for head, tail and isEmpty

--- src/test/java/javaslang/collection/ListTest.java
+++ src/test/java/javaslang/collection/ListTest.java
@@ -5,11 +5,54 @@
  */
 package javaslang.collection;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 import org.fest.assertions.api.Assertions;
 import org.junit.Test;
 
 public class ListTest {
+	
+	// -- head
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void shouldThrowWhenHeadOnEmptyList() {
+		EmptyList.instance().head();
+	}
+	
+	@Test
+	public void shouldReturnHeadOfNonEmptyList() {
+		final Integer actual = List.of(1,2,3).head();
+		assertThat(actual).isEqualTo(1);
+	}
+	
+	// -- tail
 
+	@Test(expected = UnsupportedOperationException.class)
+	public void shouldThrowWhenTailOnEmptyList() {
+		EmptyList.instance().tail();
+	}
+
+	@Test
+	public void shouldReturnTailOfNonEmptyList() {
+		final List<Integer> actual = List.of(1,2,3).tail();
+		final List<Integer> expected = List.of(2,3);
+		assertThat(actual).isEqualTo(expected);
+	}
+	
+	// -- isEmpty
+	
+	@Test
+	public void shouldRecognizeEmptyList() {
+		assertThat(List.empty().isEmpty()).isTrue();
+	}
+
+	@Test
+	public void shouldRecognizeNonEmptyList() {
+		assertThat(List.of(1).isEmpty()).isFalse();
+	}
+
+	// -- get
+	
 	@Test(expected = IndexOutOfBoundsException.class)
 	public void shouldThrowOnGetOnEmptyList() {
 		List.empty().get(0);
@@ -24,6 +67,8 @@ public class ListTest {
 	public void shouldThrowOnGetExceedingUpperBound() {
 		List.of((Object) null).get(1);
 	}
+	
+	// -- append
 	
 	@Test
 	public void shouldAppendElementToEmptyList() {
@@ -46,6 +91,7 @@ public class ListTest {
 		Assertions.assertThat(actual).isEqualTo(expected);
 	}
 
+	// -- prepend
 
 	@Test
 	public void shouldPrependElementToEmptyList() {
