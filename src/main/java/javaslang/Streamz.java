317b99eed96ac725ecf50115eca5dc0aec46d0ba
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-08-15 23:23:51 +0200

    Again, some Stream support. Seems to be necessary.

---
+++ src/main/java/javaslang/Streamz.java
@@ -0,0 +1,83 @@
+/**    / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
+package javaslang;
+
+import static javaslang.Requirements.requireNotInstantiable;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+import javaslang.Tuples.Tuple2;
+
+/**
+ * Extension methods for {@link java.util.stream.Stream}.
+ */
+public final class Streamz {
+
+	/**
+	 * This class is not intended to be instantiated.
+	 */
+	private Streamz() {
+		requireNotInstantiable();
+	}
+
+	/**
+	 * Zips the elements of a given Stream to tuples {@code (index, element)}, where index is the
+	 * occurrence within the original Stream.
+	 * 
+	 * @param stream A {@linkplain java.util.stream.Stream}.
+	 * @return A zipped stream containing elements of type {@link javaslang.Tuples.Tuple2} and
+	 *         values {@code (index, element)}. The resulting Stream is parallel if, and only if,
+	 *         the given stream is parallel.
+	 */
+	public static <T> Stream<Tuple2<Integer, T>> zipWithIndex(Stream<T> stream) {
+		final AtomicInteger index = new AtomicInteger();
+		final Function<T, Tuple2<Integer, T>> zipper = e -> Tuples.of(index.getAndIncrement(), e);
+		if (stream.isParallel()) {
+			return stream.sequential().map(zipper).parallel();
+		} else {
+			return stream.map(zipper);
+		}
+	}
+
+	/**
+	 * Convenience method for {@code Streamz.zipWithIndex(stream.boxed())}.
+	 * 
+	 * @param stream A {@linkplain java.util.stream.DoubleStream}.
+	 * @return A zipped stream with index. The original stream is boxed.
+	 * @see Streamz#zipWithIndex(Stream)
+	 */
+	public static Stream<Tuple2<Integer, Double>> zipWithIndex(DoubleStream stream) {
+		return zipWithIndex(stream.boxed());
+	}
+
+	/**
+	 * Convenience method for {@code Streamz.zipWithIndex(stream.boxed())}.
+	 * 
+	 * @param stream A {@linkplain java.util.stream.IntStream}.
+	 * @return A zipped stream with index. The original stream is boxed.
+	 * @see Streamz#zipWithIndex(Stream)
+	 */
+	public static Stream<Tuple2<Integer, Integer>> zipWithIndex(IntStream stream) {
+		return zipWithIndex(stream.boxed());
+	}
+
+	/**
+	 * Convenience method for {@code Streamz.zipWithIndex(stream.boxed())}.
+	 * 
+	 * @param stream A {@linkplain java.util.stream.LongStream}.
+	 * @return A zipped stream with index. The original stream is boxed.
+	 * @see Streamz#zipWithIndex(Stream)
+	 */
+	public static Stream<Tuple2<Integer, Long>> zipWithIndex(LongStream stream) {
+		return zipWithIndex(stream.boxed());
+	}
+
+}
---
+++ src/test/java/javaslang/StreamzTest.java
@@ -0,0 +1,44 @@
+/**    / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
+package javaslang;
+
+import static java.util.stream.Collectors.toList;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import javaslang.Tuples.Tuple2;
+
+import org.junit.Test;
+
+public class StreamzTest {
+
+	private static final String[] I_WILL_BE_BACK = { "I", "will", "be", "back!" };
+
+	@Test
+	public void shouldZipStreamWithIndex() {
+		final Stream<String> stream = Stream.of(I_WILL_BE_BACK);
+		final List<Tuple2<Integer, String>> actual = Streamz.zipWithIndex(stream).collect(toList());
+		final List<Tuple2<Integer, String>> expected = Arrays.asList(Tuples.of(0, "I"),
+				Tuples.of(1, "will"), Tuples.of(2, "be"), Tuples.of(3, "back!"));
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	@Test
+	public void shouldZipStreamWithIndexPreservingSequentialState() {
+		final Stream<String> stream = Stream.of(I_WILL_BE_BACK);
+		assertThat(Streamz.zipWithIndex(stream).isParallel()).isFalse();
+	}
+
+	@Test
+	public void shouldZipStreamWithIndexPreservingParallelState() {
+		final Stream<String> stream = Stream.of(I_WILL_BE_BACK).parallel();
+		assertThat(Streamz.zipWithIndex(stream).isParallel()).isTrue();
+	}
+
+}
