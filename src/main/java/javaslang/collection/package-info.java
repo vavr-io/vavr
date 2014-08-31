fc7af78144617ac2e3fb6e1b10f9d1a24fb85c38
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-08-31 22:16:53 +0200

    Updated package-info.

--- src/main/java/javaslang/collection/package-info.java
+++ src/main/java/javaslang/collection/package-info.java
@@ -1,6 +1,5 @@
 /**
- * Provides extension methods for java.util's Collection, List and Set.
- * TODO: mid term goal is to design a immutable collection library with mature, math. operations
+ * Contains immutable implementations of List and Tree. Provides extension methods Set.
  */
 package javaslang.collection;
 
