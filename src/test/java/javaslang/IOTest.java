966d0a83f82e6330d2d5ce2e8c764a3daaffd23b
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-09-01 11:02:29 +0200

    Covered IO 100%.

--- src/test/java/javaslang/IOTest.java
+++ src/test/java/javaslang/IOTest.java
@@ -8,8 +8,12 @@ package javaslang;
 import static javaslang.IO.UTF8;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+
+import javaslang.monad.Failure;
+import javaslang.monad.Try;
 
 import org.junit.Test;
 
@@ -48,5 +52,27 @@ public class IOTest {
 		final String actual = IO.loadResource("javaslang/resource.txt", Charset.forName("iso-8859-1")).get();
 		final String expected = " Ã¶\n( )\n/ \\";
 		assertThat(actual).isEqualTo(expected);
+	}
+
+	@Test
+	public void shouldBeAFailureWhenConvertingErrorneuousInputStreamToBytes() {
+		final Try<byte[]> actual = IO.toBytes(new ErrorneousInputStream());
+		final Try<byte[]> expected = new Failure<>(new IOException("I/O error"));
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	@Test
+	public void shouldBeAFailureWhenConvertingErrorneuousInputStreamToString() {
+		final Try<String> actual = IO.toString(new ErrorneousInputStream(), UTF8);
+		final Try<String> expected = new Failure<>(new IOException("I/O error"));
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	static class ErrorneousInputStream extends InputStream {
+
+		@Override
+		public int read() throws IOException {
+			throw new IOException("I/O error");
+		}
 	}
 }
