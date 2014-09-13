97f969f12c9e9989be510369a2ef8ea363b1cdc1
Daniel Dietrich <daniel.dietrich@gmx.net>
2014-09-13 11:32:20 +0200

    formatted

--- src/main/java/javaslang/lambda/Functions.java
+++ src/main/java/javaslang/lambda/Functions.java
@@ -192,13 +192,14 @@ public interface Functions {
 	}
 
 	@FunctionalInterface
-	static interface SerializableFunction13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, R> extends Serializable {
+	static interface SerializableFunction13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, R> extends
+			Serializable {
 
 		R apply(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13);
 
 		default SerializableFunction1<T1, SerializableFunction1<T2, SerializableFunction1<T3, SerializableFunction1<T4, SerializableFunction1<T5, SerializableFunction1<T6, SerializableFunction1<T7, SerializableFunction1<T8, SerializableFunction1<T9, SerializableFunction1<T10, SerializableFunction1<T11, SerializableFunction1<T12, SerializableFunction1<T13, R>>>>>>>>>>>>> curried() {
-			return t1 -> t2 -> t3 -> t4 -> t5 -> t6 -> t7 -> t8 -> t9 -> t10 -> t11 -> t12 -> t13 -> apply(t1, t2, t3, t4, t5,
-					t6, t7, t8, t9, t10, t11, t12, t13);
+			return t1 -> t2 -> t3 -> t4 -> t5 -> t6 -> t7 -> t8 -> t9 -> t10 -> t11 -> t12 -> t13 -> apply(t1, t2, t3,
+					t4, t5, t6, t7, t8, t9, t10, t11, t12, t13);
 		}
 
 		default SerializableFunction1<Tuples.Tuple13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>, R> tupled() {
