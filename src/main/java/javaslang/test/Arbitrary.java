541306ad4682be32587841deba3285e581428be0
Daniel Dietrich <daniel.dietrich@gmx.net>
2015-01-23 11:43:28 +0100

    Towards #72: More mature Arbitrary interface. Streamable generators. Fixed flatMap.

--- src/main/java/javaslang/test/Arbitrary.java
+++ src/main/java/javaslang/test/Arbitrary.java
@@ -5,20 +5,64 @@
  */
 package javaslang.test;
 
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+
 /**
  * Represents an arbitrary object of type T.
  *
  * @param <T> The type of the arbitrary object.
  */
 @FunctionalInterface
-public interface Arbitrary<T> {
+public interface Arbitrary<T> extends IntFunction<Gen<T>>, Function<Integer, Gen<T>> {
 
     /**
+     * <p>
+     * Returns an arbitrary object of
      * Use {@link Gen} to generate arbitrary objects.
+     * </p>
+     * Example:
+     * <pre>
+     * <code>
+     * // represents arbitrary binary trees of a certain depth n
+     * final class ArbitraryTree implements Arbitrary&lt;BinaryTree&lt;Integer&gt;&gt; {
+     *     @Override
+     *     public Gen&lt;BinaryTree&lt;Integer&gt;&gt; apply(int n) {
+     *         return Gen.choose(-1000, 1000).flatMap(value -> {
+     *                  if (n == 0) {
+     *                      return Gen.of(BinaryTree.leaf(value));
+     *                  } else {
+     *                      return Gen.frequency(
+     *                              Tuple.of(1, Gen.of(BinaryTree.leaf(value))),
+     *                              Tuple.of(4, Gen.of(BinaryTree.branch(apply(n / 2).get(), value, apply(n / 2).get())))
+     *                      );
+     *                  }
+     *         });
+     *     }
+     * }
+     *
+     * // tree generator with a size hint of 10
+     * final Gen&lt;BinaryTree&lt;Integer&gt;&gt; treeGen = new ArbitraryTree().apply(10);
+     *
+     * // stream sum of tree node values to console for 100 arbitrary trees
+     * Stream.of(treeGen).map(Tree::sum).take(100).stdout();
+     * </code>
+     * </pre>
      *
      * @param size A size parameter which may be interpreted idividually and is constant for all arbitrary objects regarding one property check.
      * @return A generator for objects of type T.
      */
-    Gen<T> arbitrary(int size);
+    @Override
+    Gen<T> apply(int size);
 
+    /**
+     * Calls {@link #apply(int)}.
+     * @param size A size hint.
+     * @return The result of {@link #apply(int)}.
+     */
+    default Gen<T> apply(Integer size) {
+        Objects.requireNonNull(size, "size is null");
+        return apply((int) size);
+    }
 }
--- src/test/java/javaslang/test/ArbitraryTest.java
+++ src/test/java/javaslang/test/ArbitraryTest.java
@@ -7,21 +7,23 @@ package javaslang.test;
 
 import javaslang.Tuple;
 import javaslang.collection.BinaryTree;
-import javaslang.collection.List;
+import javaslang.collection.Stream;
 
 public class ArbitraryTest {
 
     public static void main(String[] args) {
 
-        final class ArbitraryTree {
-            Gen<BinaryTree<Integer>> gen(int n) {
-                return Gen.choose(0, 1000).flatMap(i -> {
+        // represents arbitrary binary trees of a certain depth n
+        final class ArbitraryTree implements Arbitrary<BinaryTree<Integer>> {
+            @Override
+            public Gen<BinaryTree<Integer>> apply(int n) {
+                return Gen.choose(-1000, 1000).flatMap(value -> {
                             if (n == 0) {
-                                return Gen.of(BinaryTree.leaf(i));
+                                return Gen.of(BinaryTree.leaf(value));
                             } else {
                                 return Gen.frequency(
-                                        Tuple.of(1, Gen.of(BinaryTree.leaf(i))),
-                                        Tuple.of(4, Gen.of(BinaryTree.branch(gen(n/2).get(), i, gen(n/2).get())))
+                                        Tuple.of(1, Gen.of(BinaryTree.leaf(value))),
+                                        Tuple.of(4, Gen.of(BinaryTree.branch(apply(n / 2).get(), value, apply(n / 2).get())))
                                 );
                             }
                         }
@@ -29,10 +31,10 @@ public class ArbitraryTest {
             }
         }
 
-        final Arbitrary<BinaryTree<Integer>> tree = n -> new ArbitraryTree().gen(n);
+        // tree generator with a size hint of 10
+        final Gen<BinaryTree<Integer>> treeGen = new ArbitraryTree().apply(10);
 
-        // TODO: DELME
-        List.range(0, 10).forEach(i ->  System.out.println(tree.arbitrary(i).get()));
-
+        // stream sum of tree node values to console for 100 arbitrary trees
+        Stream.of(treeGen)/*TODO:.map(Tree::sum)*/.take(100).stdout();
     }
 }
