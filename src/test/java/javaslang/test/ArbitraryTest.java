f60551569a0ced790a9ea3c93b0818b59b06514b
Daniel Dietrich <daniel.dietrich@gmx.net>
2015-02-07 08:44:47 +0100

    Towards #97: Test of type Arbitrary

--- src/test/java/javaslang/test/ArbitraryTest.java
+++ src/test/java/javaslang/test/ArbitraryTest.java
@@ -7,39 +7,61 @@ package javaslang.test;
 
 import javaslang.Tuple;
 import javaslang.collection.BinaryTree;
-import javaslang.collection.Stream;
+import org.junit.Test;
 
 import java.util.Random;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ArbitraryTest {
 
-    // TODO: unit tests
+    @Test
+    public void shouldFlatMapArbitrary() {
+        final Arbitrary<Integer> arbitraryInt = size -> Gen.choose(-size, size);
+        final Arbitrary<BinaryTree<Integer>> arbitraryTree = arbitraryInt.flatMap(i -> new ArbitraryBinaryTree(-i, i));
+        assertThat(arbitraryTree.apply(0).apply(new Random())).isNotNull();
+    }
 
-    public static void main(String[] args) {
+    @Test
+    public void shouldMapArbitrary() {
+        final Arbitrary<Integer> arbitraryInt = size -> Gen.choose(-size, size);
+        final Arbitrary<BinaryTree<Integer>> arbitraryTree = arbitraryInt.map(BinaryTree::leaf);
+        assertThat(arbitraryTree.apply(0).apply(new Random())).isNotNull();
+    }
 
-        // represents arbitrary binary trees of a certain depth n
-        final class ArbitraryTree implements Arbitrary<BinaryTree<Integer>> {
-            @Override
-            public Gen<BinaryTree<Integer>> apply(int n) {
-                return random -> Gen.choose(-1000, 1000).flatMap(value -> {
-                            if (n == 0) {
-                                return Gen.of(BinaryTree.leaf(value));
-                            } else {
-                                return Gen.frequency(
-                                        Tuple.of(1, Gen.of(BinaryTree.leaf(value))),
-                                        Tuple.of(4, Gen.of(BinaryTree.branch(apply(n / 2).apply(random), value, apply(n / 2).apply(random))))
-                                );
-                            }
-                        }
-                ).apply(random);
-            }
+    @Test
+    public void shouldFilterArbitrary() {
+        final Arbitrary<BinaryTree<Integer>> arbitraryTree = new ArbitraryBinaryTree(0, 1000);
+        final Arbitrary<BinaryTree<Integer>> arbitraryTreeWithEvenNodeCount = arbitraryTree.filter(tree -> tree.nodeCount() % 2 == 0);
+        assertThat(arbitraryTree.apply(0).apply(new Random())).isNotNull();
+    }
+
+    /**
+     * Represents arbitrary binary trees of a certain depth n with values of type int.
+     */
+    static class ArbitraryBinaryTree implements Arbitrary<BinaryTree<Integer>> {
+
+        final int minValue;
+        final int maxValue;
+
+        ArbitraryBinaryTree(int minValue, int maxValue) {
+            this.minValue = Math.min(minValue, maxValue);
+            this.maxValue = Math.max(minValue, maxValue);
         }
 
-        // tree generator with a size hint of 10
-        final Gen<BinaryTree<Integer>> treeGen = new ArbitraryTree().apply(10);
-
-        // stream sum of tree node values to console for 100 arbitrary trees
-        final Random rng = new Random();
-        Stream.gen(() -> treeGen.apply(rng))/*TODO:.map(Tree::sum)*/.take(100).stdout();
+        @Override
+        public Gen<BinaryTree<Integer>> apply(int n) {
+            return random -> Gen.choose(minValue, maxValue).flatMap(value -> {
+                        if (n == 0) {
+                            return Gen.of(BinaryTree.leaf(value));
+                        } else {
+                            return Gen.frequency(
+                                    Tuple.of(1, Gen.of(BinaryTree.leaf(value))),
+                                    Tuple.of(4, Gen.of(BinaryTree.branch(apply(n / 2).apply(random), value, apply(n / 2).apply(random))))
+                            );
+                        }
+                    }
+            ).apply(random);
+        }
     }
 }
