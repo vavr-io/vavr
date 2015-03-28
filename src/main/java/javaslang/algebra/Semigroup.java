ca7137287e308a90eaf85b65e94cbf9a45d7b267
Daniel Dietrich <daniel.dietrich@gmx.net>
2015-03-28 23:33:15 +0100

    Towards #97: javadoc of Semigroup

--- src/main/java/javaslang/algebra/Semigroup.java
+++ src/main/java/javaslang/algebra/Semigroup.java
@@ -6,20 +6,26 @@
 package javaslang.algebra;
 
 /**
- * A Semigroup is an algebraic structure consisting of
- *
+ * <p>A Semigroup is a type with an associative binary operation {@linkplain #combine(Object, Object)}.</p>
+ * <p>Given a type {@code A}, instances of Semigroup should satisfy the following law:</p>
  * <ul>
- *     <li>Some type A</li>
- *     <li>An associative binary operation {@code combine}, such that {@code combine(combine(x,y),z) == combine(x,combine(y,z))} for any x,y,z of type A.</li>
+ * <li>Associativity: {@code combine(combine(x,y),z) == combine(x,combine(y,z))} for any {@code x,y,z} of type
+ * {@code A}.</li>
  * </ul>
- *
- * Technically a Semigroup is the same as a {@code java.util.function.BiFunction<A,A,A>}. Introducing this new type
- * clarifies that the operation {@code combine} is associative.
+ * <p>Note: Technically a Semigroup is the same as a {@code java.util.function.BiFunction<A,A,A>}. Introducing this new type
+ * clarifies that the operation {@code combine} is associative.</p>
  *
  * @param <A> A type.
  */
 @FunctionalInterface
 public interface Semigroup<A> {
 
+    /**
+     * Combines two elements of the same type, which is also returned.
+     *
+     * @param a1 An element
+     * @param a2 Another element
+     * @return The combination of a1 and a2
+     */
     A combine(A a1, A a2);
 }
