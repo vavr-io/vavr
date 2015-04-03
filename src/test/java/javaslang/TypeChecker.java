3221fe3ece376bad71030e5d5ea023af79f06eef
Daniel Dietrich <daniel.dietrich@gmx.net>
2015-04-03 02:28:11 +0200

    Towards #121: todos

--- src/test/java/javaslang/TypeChecker.java
+++ src/test/java/javaslang/TypeChecker.java
@@ -22,6 +22,7 @@ public class TypeChecker {
 
     //@Test
     public void shouldHaveAConsistentTypeSystem() {
+        // TODO: check(BinaryTree.class, RoseTree.class, List.class, Stream.class, Success.class, Failure.class, Left.class, Right.class, None.class, Some.class, Monoid.class, Monad1.class, ...)
         final Class<?> clazz = Seq.class;
         final Traversable<Method> unoverriddenMethods = getUnoverriddenMethods(clazz);
         if (!unoverriddenMethods.isEmpty()) {
@@ -39,29 +40,28 @@ public class TypeChecker {
         if (superClasses.isEmpty()) {
             return Stream.nil();
         } else {
-            final Traversable<ComparableMethod> superMethods = getMethods(superClasses)
-                    .filter(comparableMethod ->
-                            // We're interested in methods that should be overridden with actual type as return type.
-                            // Because we check this recursively, the class hierarchy is consistent here.
-                            comparableMethod.m.getDeclaringClass().equals(comparableMethod.m.getReturnType()));
-            final Traversable<ComparableMethod> thisMethods = getMethods(Stream.of(clazz));
+            final Traversable<ComparableMethod> superMethods = getRelevantMethods(superClasses).filter(comparableMethod ->
+                    // We're interested in methods that should be overridden with actual type as return type.
+                    // Because we check this recursively, the class hierarchy is consistent here.
+                    comparableMethod.m.getDeclaringClass().equals(comparableMethod.m.getReturnType()));
+            final Traversable<ComparableMethod> thisMethods = getRelevantMethods(Stream.of(clazz));
             return superMethods.filter(superMethod -> thisMethods
-                    .findFirst(thisMethod -> thisMethod.equals(superMethod))
-                    // TODO: special case if visibility is package private and classes are in different package
-                    .map(thisMethod -> !thisMethod.m.getDeclaringClass().equals(thisMethod.m.getReturnType()))
-                    .orElse(true))
+                        .findFirst(thisMethod -> thisMethod.equals(superMethod))
+                        // TODO: special case if visibility is package private and classes are in different package
+                        .map(thisMethod -> !thisMethod.m.getDeclaringClass().equals(thisMethod.m.getReturnType()))
+                        .orElse(true))
                     .map(comparableMethod -> comparableMethod.m);
         }
     }
 
     // TODO: change Traversable to Seq after running TypeChecker and fixing findings
-    Traversable<ComparableMethod> getMethods(Traversable<Class<?>> classes) {
+    Traversable<ComparableMethod> getRelevantMethods(Traversable<Class<?>> classes) {
         return classes
                 .flatMap(clazz ->
                         Stream.of(clazz.getDeclaredMethods()).filter((Method m) ->
                                 // https://javax0.wordpress.com/2014/02/26/syntethic-and-bridge-methods/
                                 !m.isBridge() && !m.isSynthetic() &&
-                                        // private and static methods cannot be overridden
+                                // private and static methods cannot be overridden
                                 !Modifier.isPrivate(m.getModifiers()) && !Modifier.isStatic(m.getModifiers()))
                             .map(ComparableMethod::new));
     }
