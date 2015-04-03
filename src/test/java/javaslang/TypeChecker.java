31628f831cf4e9939a47b7ea997323abe2318299
Daniel Dietrich <daniel.dietrich@gmx.net>
2015-04-03 15:42:55 +0200

    Towards #121: fixes unoverridden method finder + some todos

--- src/test/java/javaslang/TypeChecker.java
+++ src/test/java/javaslang/TypeChecker.java
@@ -5,31 +5,32 @@
  */
 package javaslang;
 
-import javaslang.collection.List;
-import javaslang.collection.Seq;
-import javaslang.collection.Stream;
-import javaslang.collection.Traversable;
+import javaslang.algebra.Monad1;
+import javaslang.algebra.Monoid;
+import javaslang.collection.*;
+import javaslang.control.*;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class TypeChecker {
 
-    public static void main(String[] args) {
-        new TypeChecker().shouldHaveAConsistentTypeSystem();
-    }
-
-    //@Test
-    public void shouldHaveAConsistentTypeSystem() {
-        // TODO: check(BinaryTree.class, RoseTree.class, List.class, Stream.class, Success.class, Failure.class, Left.class, Right.class, None.class, Some.class, Monoid.class, Monad1.class, ...)
-        final Class<?> clazz = Seq.class;
-        final Traversable<Method> unoverriddenMethods = getUnoverriddenMethods(clazz);
-        if (!unoverriddenMethods.isEmpty()) {
-            final String msg = String.format("%s has to override the following methods with return type %s:\n%s",
-                    clazz.getName(), clazz.getSimpleName(), unoverriddenMethods.map(Object::toString).join("\n"));
-            // TODO: throw new AssertionError(msg);
-            System.out.println(msg);
+    @Test
+    @Ignore
+    public void shouldHaveAConsistentTypeSystem() throws Exception {
+        // TODO: recursively check the type hierarchy (e.g. Seq in the case of List and Stream - but remember visited classes)
+        // TODO: perform this check for *all* Javaslang classes (src and src-gen)
+        final Stream<String> msgs = Stream.of(BinaryTree.class, RoseTree.class, List.class, Stream.class, Success.class, Failure.class, Left.class, Right.class, None.class, Some.class, Monoid.class, Monad1.class)
+                .map(clazz -> Tuple.of(clazz, getUnoverriddenMethods(clazz)))
+                .filter(findings -> !findings._2.isEmpty())
+                .map(findings -> String.format("%s has to override the following methods with return type %s:\n%s",
+                        findings._1.getName(), findings._1.getSimpleName(), findings._2.map(Object::toString).join("\n")));
+        if (!msgs.isEmpty()) {
+            throw new AssertionError(msgs.join("\n\n", "Unoverriden methods found.\n", ""));
         }
     }
 
@@ -40,30 +41,34 @@ public class TypeChecker {
         if (superClasses.isEmpty()) {
             return Stream.nil();
         } else {
-            final Traversable<ComparableMethod> superMethods = getRelevantMethods(superClasses).filter(comparableMethod ->
+            final Traversable<ComparableMethod> superMethods = getOverridableMethods(superClasses).filter(comparableMethod ->
                     // We're interested in methods that should be overridden with actual type as return type.
                     // Because we check this recursively, the class hierarchy is consistent here.
                     comparableMethod.m.getDeclaringClass().equals(comparableMethod.m.getReturnType()));
-            final Traversable<ComparableMethod> thisMethods = getRelevantMethods(Stream.of(clazz));
+            final Traversable<ComparableMethod> thisMethods = getOverridableMethods(Stream.of(clazz));
             return superMethods.filter(superMethod -> thisMethods
-                        .findFirst(thisMethod -> thisMethod.equals(superMethod))
-                        // TODO: special case if visibility is package private and classes are in different package
-                        .map(thisMethod -> !thisMethod.m.getDeclaringClass().equals(thisMethod.m.getReturnType()))
-                        .orElse(true))
-                    .map(comparableMethod -> comparableMethod.m);
+                    .findFirst(thisMethod -> thisMethod.equals(superMethod))
+                            // TODO: special case if visibility is package private and classes are in different package
+                    .map(thisMethod -> !clazz.equals(thisMethod.m.getReturnType()))
+                    .orElse(true))
+                    .map(comparableMethod -> comparableMethod.m)
+                    // TODO: .sort()
+                    ;
         }
     }
 
     // TODO: change Traversable to Seq after running TypeChecker and fixing findings
-    Traversable<ComparableMethod> getRelevantMethods(Traversable<Class<?>> classes) {
+    Traversable<ComparableMethod> getOverridableMethods(Traversable<Class<?>> classes) {
         return classes
                 .flatMap(clazz ->
-                        Stream.of(clazz.getDeclaredMethods()).filter((Method m) ->
-                                // https://javax0.wordpress.com/2014/02/26/syntethic-and-bridge-methods/
-                                !m.isBridge() && !m.isSynthetic() &&
-                                // private and static methods cannot be overridden
-                                !Modifier.isPrivate(m.getModifiers()) && !Modifier.isStatic(m.getModifiers()))
-                            .map(ComparableMethod::new));
+                    Stream.of(clazz.getDeclaredMethods()).filter((Method m) ->
+                            // https://javax0.wordpress.com/2014/02/26/syntethic-and-bridge-methods/
+                            !m.isBridge() && !m.isSynthetic() &&
+                            // private, static and final methods cannot be overridden
+                            !Modifier.isPrivate(m.getModifiers()) && !Modifier.isFinal(m.getModifiers()) && !Modifier.isStatic(m.getModifiers()) &&
+                            // we also don't want to cope with methods declared in Object
+                            !m.getDeclaringClass().equals(Object.class))
+                        .map(ComparableMethod::new));
     }
 
     static class ComparableMethod {
@@ -81,7 +86,7 @@ public class TypeChecker {
             } else if (o instanceof ComparableMethod) {
                 final ComparableMethod that = (ComparableMethod) o;
                 return Objects.equals(this.m.getName(), that.m.getName()) &&
-                        Objects.equals(this.m.getParameterTypes(), that.m.getParameterTypes());
+                        Arrays.equals(this.m.getParameterTypes(), that.m.getParameterTypes());
             } else {
                 return false;
             }
@@ -89,7 +94,7 @@ public class TypeChecker {
 
         @Override
         public int hashCode() {
-            return Objects.hash(m.getName(), m.getParameterTypes());
+            return Objects.hash(m.getName(), Arrays.hashCode(m.getParameterTypes()));
         }
 
         @Override
