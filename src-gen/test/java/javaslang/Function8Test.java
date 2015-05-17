aaca75f1ef105c2f4b3cf79314eec0e56bf8e199
Daniel Dietrich <daniel.dietrich@gmx.net>
2015-05-17 03:08:49 +0200

    missing generated files

---
+++ src-gen/test/java/javaslang/Function8Test.java
@@ -0,0 +1,105 @@
+/*     / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014-2015 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
+package javaslang;
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*\
+   G E N E R A T O R   C R A F T E D
+\*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class Function8Test {
+
+    @Test
+    public void shouldLift() {
+        class Type {
+            Object methodReference(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8) {
+                return null;
+            }
+        }
+        final Type type = new Type();
+        assertThat(Function8.lift(type::methodReference)).isNotNull();
+    }
+
+    @Test
+    public void shouldPartiallyApplyWith1Arguments() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.apply(null)).isNotNull();
+    }
+
+    @Test
+    public void shouldPartiallyApplyWith2Arguments() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.apply(null, null)).isNotNull();
+    }
+
+    @Test
+    public void shouldPartiallyApplyWith3Arguments() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.apply(null, null, null)).isNotNull();
+    }
+
+    @Test
+    public void shouldPartiallyApplyWith4Arguments() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.apply(null, null, null, null)).isNotNull();
+    }
+
+    @Test
+    public void shouldPartiallyApplyWith5Arguments() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.apply(null, null, null, null, null)).isNotNull();
+    }
+
+    @Test
+    public void shouldPartiallyApplyWith6Arguments() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.apply(null, null, null, null, null, null)).isNotNull();
+    }
+
+    @Test
+    public void shouldPartiallyApplyWith7Arguments() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.apply(null, null, null, null, null, null, null)).isNotNull();
+    }
+
+    @Test
+    public void shouldGetArity() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.arity()).isEqualTo(8);
+    }
+
+    @Test
+    public void shouldCurry() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        final Function1<Object, Function1<Object, Function1<Object, Function1<Object, Function1<Object, Function1<Object, Function1<Object, Function1<Object, Object>>>>>>>> curried = f.curried();
+        assertThat(curried).isNotNull();
+    }
+
+    @Test
+    public void shouldTuple() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        final Function1<Tuple8<Object, Object, Object, Object, Object, Object, Object, Object>, Object> tupled = f.tupled();
+        assertThat(tupled).isNotNull();
+    }
+
+    @Test
+    public void shouldReverse() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        assertThat(f.reversed()).isNotNull();
+    }
+
+    @Test
+    public void shouldComposeWithAndThen() {
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> f = (o1, o2, o3, o4, o5, o6, o7, o8) -> null;
+        final Function1<Object, Object> after = o -> null;
+        final Function8<Object, Object, Object, Object, Object, Object, Object, Object, Object> composed = f.andThen(after);
+        assertThat(composed).isNotNull();
+    }
+
+}
---
+++ src-gen/test/java/javaslang/test/PropertyCheck7Test.java
@@ -0,0 +1,107 @@
+/*     / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014-2015 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
+package javaslang.test;
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*\
+   G E N E R A T O R   C R A F T E D
+\*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javaslang.CheckedFunction7;
+import org.junit.Test;
+
+public class PropertyCheck7Test {
+
+    static final Arbitrary<Object> OBJECTS = Gen.of(null).arbitrary();
+
+    @Test
+    public void shouldApplyForAllOfArity7() {
+        final Property.ForAll7<Object, Object, Object, Object, Object, Object, Object> forAll = new Property("test").forAll(null, null, null, null, null, null, null);
+        assertThat(forAll).isNotNull();
+    }
+
+    @Test
+    public void shouldApplySuchThatOfArity7() {
+        final Property.ForAll7<Object, Object, Object, Object, Object, Object, Object> forAll = new Property("test").forAll(OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS);
+        final CheckedFunction7<Object, Object, Object, Object, Object, Object, Object, Boolean> predicate = (o1, o2, o3, o4, o5, o6, o7) -> true;
+        final Property.Property7<Object, Object, Object, Object, Object, Object, Object> suchThat = forAll.suchThat(predicate);
+        assertThat(suchThat).isNotNull();
+    }
+
+    @Test
+    public void shouldCheckTrueProperty7() {
+        final Property.ForAll7<Object, Object, Object, Object, Object, Object, Object> forAll = new Property("test").forAll(OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS);
+        final CheckedFunction7<Object, Object, Object, Object, Object, Object, Object, Boolean> predicate = (o1, o2, o3, o4, o5, o6, o7) -> true;
+        final CheckResult result = forAll.suchThat(predicate).check();
+        assertThat(result.isSatisfied()).isTrue();
+        assertThat(result.isExhausted()).isFalse();
+    }
+
+    @Test
+    public void shouldCheckFalseProperty7() {
+        final Property.ForAll7<Object, Object, Object, Object, Object, Object, Object> forAll = new Property("test").forAll(OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS);
+        final CheckedFunction7<Object, Object, Object, Object, Object, Object, Object, Boolean> predicate = (o1, o2, o3, o4, o5, o6, o7) -> false;
+        final CheckResult result = forAll.suchThat(predicate).check();
+        assertThat(result.isFalsified()).isTrue();
+    }
+
+    @Test
+    public void shouldCheckErroneousProperty7() {
+        final Property.ForAll7<Object, Object, Object, Object, Object, Object, Object> forAll = new Property("test").forAll(OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS);
+        final CheckedFunction7<Object, Object, Object, Object, Object, Object, Object, Boolean> predicate = (o1, o2, o3, o4, o5, o6, o7) -> { throw new RuntimeException("woops"); };
+        final CheckResult result = forAll.suchThat(predicate).check();
+        assertThat(result.isErroneous()).isTrue();
+    }
+
+    @Test
+    public void shouldCheckProperty7ImplicationWithTruePrecondition() {
+        final Property.ForAll7<Object, Object, Object, Object, Object, Object, Object> forAll = new Property("test").forAll(OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS);
+        final CheckedFunction7<Object, Object, Object, Object, Object, Object, Object, Boolean> p1 = (o1, o2, o3, o4, o5, o6, o7) -> true;
+        final CheckedFunction7<Object, Object, Object, Object, Object, Object, Object, Boolean> p2 = (o1, o2, o3, o4, o5, o6, o7) -> true;
+        final CheckResult result = forAll.suchThat(p1).implies(p2).check();
+        assertThat(result.isSatisfied()).isTrue();
+        assertThat(result.isExhausted()).isFalse();
+    }
+
+    @Test
+    public void shouldCheckProperty7ImplicationWithFalsePrecondition() {
+        final Property.ForAll7<Object, Object, Object, Object, Object, Object, Object> forAll = new Property("test").forAll(OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS);
+        final CheckedFunction7<Object, Object, Object, Object, Object, Object, Object, Boolean> p1 = (o1, o2, o3, o4, o5, o6, o7) -> false;
+        final CheckedFunction7<Object, Object, Object, Object, Object, Object, Object, Boolean> p2 = (o1, o2, o3, o4, o5, o6, o7) -> true;
+        final CheckResult result = forAll.suchThat(p1).implies(p2).check();
+        assertThat(result.isSatisfied()).isTrue();
+        assertThat(result.isExhausted()).isTrue();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowOnProperty7CheckGivenNegativeTries() {
+        new Property("test")
+            .forAll(OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS)
+            .suchThat((o1, o2, o3, o4, o5, o6, o7) -> true)
+            .check(Checkable.RNG.get(), 0, -1);
+    }
+
+    @Test
+    public void shouldReturnErroneousProperty7CheckResultIfGenFails() {
+        final Arbitrary<Object> failingGen = Gen.fail("woops").arbitrary();
+        final CheckResult result = new Property("test")
+            .forAll(failingGen, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS)
+            .suchThat((o1, o2, o3, o4, o5, o6, o7) -> true)
+            .check();
+        assertThat(result.isErroneous()).isTrue();
+    }
+
+    @Test
+    public void shouldReturnErroneousProperty7CheckResultIfArbitraryFails() {
+        final Arbitrary<Object> failingArbitrary = size -> { throw new RuntimeException("woops"); };
+        final CheckResult result = new Property("test")
+            .forAll(failingArbitrary, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS, OBJECTS)
+            .suchThat((o1, o2, o3, o4, o5, o6, o7) -> true)
+            .check();
+        assertThat(result.isErroneous()).isTrue();
+    }
+}
