463a78661a81cec3b95f16e9df3a0f849d412f6e
Ruslan Sennov <ruslan.sennov@gmail.com>
2015-07-26 16:51:56 +0300

    #126 Implement the first 99 Project Euler problems using javaslang collections
    Solutions for problems 29, 57.

--- src/test/java/javaslang/collection/euler/Euler48.java
+++ src/test/java/javaslang/collection/euler/Euler48.java
@@ -18,6 +18,7 @@ public class Euler48 {
      * <strong>Problem 48: Self powers</strong>
      * <p>The series, 1<sup>1</sup> + 2<sup>2</sup> + 3<sup>3</sup> + ... + 10<sup>10</sup> = 10405071317.</p>
      * <p>Find the last ten digits of the series, 1<sup>1</sup> + 2<sup>2</sup> + 3<sup>3</sup> + ... + 1000<sup>1000</sup>.</p>
+     * <p>See also <a href="https://projecteuler.net/problem=48">projecteuler.net problem 48</a>.</p>
      */
     @Test
     public void shouldSolveProblem48() {
---
+++ src/test/java/javaslang/collection/euler/Euler57.java
@@ -0,0 +1,55 @@
+/*     / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014-2015 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
+package javaslang.collection.euler;
+
+import javaslang.Tuple;
+import javaslang.Tuple2;
+import javaslang.collection.Stream;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+public class Euler57 {
+
+    /**
+     * <strong>Problem 57: Square root convergents</strong>
+     * <p>It is possible to show that the square root of two can be expressed as an infinite continued fraction.</p>
+     * <p>√ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...</p>
+     * <p>By expanding this for the first four iterations, we get:</p>
+     * <p>1 + 1/2 = 3/2 = 1.5</p>
+     * <p>1 + 1/(2 + 1/2) = 7/5 = 1.4</p>
+     * <p>1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...</p>
+     * <p>1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...</p>
+     * <p>The next three expansions are 99/70, 239/169, and 577/408, but the eighth expansion, 1393/985, is the first example where the number of digits in the numerator exceeds the number of digits in the denominator.</p>
+     * <p>In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?</p>
+     * <p>See also <a href="https://projecteuler.net/problem=57">projecteuler.net problem 57</a>.</p>
+     */
+    @Test
+    public void shouldSolveProblem57() {
+        assertThat(cnt()).isEqualTo(153);
+    }
+
+    private static int cnt() {
+        return fractions()
+                .take(1000)
+                .filter(f -> f._1.toPlainString().length() > f._2.toPlainString().length())
+                .length();
+    }
+
+    private static Stream<Tuple2<BigDecimal,BigDecimal>> fractions() {
+        return Stream.gen(Tuple.of(BigDecimal.ONE, BigDecimal.ONE), Euler57::it);
+    }
+
+    /**
+     * a/b -> 1 + 1/(1 + a/b) = (a+2b)/(a+b)
+     */
+    private static Tuple2<BigDecimal,BigDecimal> it(Tuple2<BigDecimal,BigDecimal> val) {
+        return Tuple.of(val._1.add(val._2.add(val._2)), val._1.add(val._2));
+    }
+}
