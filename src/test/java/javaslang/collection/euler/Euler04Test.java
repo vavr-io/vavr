ddf1117434e14a5e8061cb4b3624fa389bfe4645
Ruslan Sennov <ruslan.sennov@gmail.com>
2015-07-28 20:50:27 +0300

    ooops... euler04 fixed

--- src/test/java/javaslang/collection/euler/Euler04Test.java
+++ src/test/java/javaslang/collection/euler/Euler04Test.java
@@ -30,7 +30,7 @@ public class Euler04Test {
 
     private static int largestPalindromeOfProductsFromFactorsInRange(final int min, final int max) {
         return Utils.cross(List.rangeClosed(min, max), List.rangeClosed(min, max))
-                .filter(t -> t._1 < t._2)
+                .filter(t -> t._1 <= t._2)
                 .map(t -> t._1 * t._2)
                 .filter(Euler04Test::isPalindrome)
                 .max().get();
