f9a716d134e4ccf0a5336f8055e722ffe426becb
Daniel Dietrich <daniel.dietrich@gmx.net>
2015-08-04 01:14:11 +0200

    Added interface LinearSeq and IndexedSeq

---
+++ src/main/java/javaslang/collection/IndexedSeq.java
@@ -0,0 +1,201 @@
+/*     / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014-2015 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
+package javaslang.collection;
+
+import javaslang.Kind;
+import javaslang.Tuple2;
+import javaslang.control.Option;
+import javaslang.control.Some;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+/**
+ * Interface for indexed sequences.
+ * <p>
+ * Efficient random access is characteristic for indexed sequences.
+ *
+ * @param <T> component type
+ */
+public interface IndexedSeq<T> extends Seq<T> {
+
+    // -- Adjusted return types of Seq methods
+
+    @Override
+    IndexedSeq<T> append(T element);
+
+    @Override
+    IndexedSeq<T> appendAll(Iterable<? extends T> elements);
+
+    @Override
+    IndexedSeq<T> clear();
+
+    @Override
+    IndexedSeq<Tuple2<T, T>> cartesianProduct();
+
+    @Override
+    <U> IndexedSeq<Tuple2<T, U>> cartesianProduct(Iterable<? extends U> that);
+
+    @Override
+    IndexedSeq<? extends IndexedSeq<T>> combinations();
+
+    @Override
+    IndexedSeq<? extends IndexedSeq<T>> combinations(int k);
+
+    @Override
+    IndexedSeq<T> distinct();
+
+    @Override
+    <U> IndexedSeq<T> distinct(Function<? super T, ? extends U> keyExtractor);
+
+    @Override
+    IndexedSeq<T> drop(int n);
+
+    @Override
+    IndexedSeq<T> dropRight(int n);
+
+    @Override
+    IndexedSeq<T> dropWhile(Predicate<? super T> predicate);
+
+    @Override
+    IndexedSeq<T> filter(Predicate<? super T> predicate);
+
+    @Override
+    IndexedSeq<Some<T>> filterOption(Predicate<? super T> predicate);
+
+    @Override
+    IndexedSeq<T> findAll(Predicate<? super T> predicate);
+
+    @Override
+    <U> IndexedSeq<U> flatMap(Function<? super T, ? extends Iterable<? extends U>> mapper);
+
+    @Override
+    <U> IndexedSeq<U> flatMapM(Function<? super T, ? extends Kind<? extends IterableKind<?>, ? extends U>> mapper);
+
+    @Override
+    <U> IndexedSeq<U> flatten(Function<? super T, ? extends Iterable<? extends U>> f);
+
+    @Override
+    <U> IndexedSeq<U> flattenM(Function<? super T, ? extends Kind<? extends IterableKind<?>, ? extends U>> f);
+
+    @Override
+    IndexedSeq<? extends IndexedSeq<T>> grouped(int size);
+
+    @Override
+    IndexedSeq<T> init();
+
+    @Override
+    Option<? extends IndexedSeq<T>> initOption();
+
+    @Override
+    IndexedSeq<T> insert(int index, T element);
+
+    @Override
+    IndexedSeq<T> insertAll(int index, Iterable<? extends T> elements);
+
+    @Override
+    IndexedSeq<T> intersperse(T element);
+
+    @Override
+    <U> IndexedSeq<U> map(Function<? super T, ? extends U> mapper);
+
+    @Override
+    Tuple2<? extends IndexedSeq<T>, ? extends IndexedSeq<T>> partition(Predicate<? super T> predicate);
+
+    @Override
+    IndexedSeq<T> peek(Consumer<? super T> action);
+
+    @Override
+    IndexedSeq<? extends IndexedSeq<T>> permutations();
+
+    @Override
+    IndexedSeq<T> prepend(T element);
+
+    @Override
+    IndexedSeq<T> prependAll(Iterable<? extends T> elements);
+
+    @Override
+    IndexedSeq<T> remove(T element);
+
+    @Override
+    IndexedSeq<T> removeAll(T element);
+
+    @Override
+    IndexedSeq<T> removeAll(Iterable<? extends T> elements);
+
+    @Override
+    IndexedSeq<T> replace(T currentElement, T newElement);
+
+    @Override
+    IndexedSeq<T> replaceAll(T currentElement, T newElement);
+
+    @Override
+    IndexedSeq<T> replaceAll(UnaryOperator<T> operator);
+
+    @Override
+    IndexedSeq<T> retainAll(Iterable<? extends T> elements);
+
+    @Override
+    IndexedSeq<T> reverse();
+
+    @Override
+    IndexedSeq<T> set(int index, T element);
+
+    @Override
+    IndexedSeq<? extends IndexedSeq<T>> sliding(int size);
+
+    @Override
+    IndexedSeq<? extends IndexedSeq<T>> sliding(int size, int step);
+
+    @Override
+    IndexedSeq<T> sort();
+
+    @Override
+    IndexedSeq<T> sort(Comparator<? super T> comparator);
+
+    @Override
+    Tuple2<? extends IndexedSeq<T>, ? extends IndexedSeq<T>> span(Predicate<? super T> predicate);
+
+    @Override
+    IndexedSeq<T> subsequence(int beginIndex);
+
+    @Override
+    IndexedSeq<T> subsequence(int beginIndex, int endIndex);
+
+    @Override
+    IndexedSeq<T> tail();
+
+    @Override
+    Option<? extends IndexedSeq<T>> tailOption();
+
+    @Override
+    IndexedSeq<T> take(int n);
+
+    @Override
+    IndexedSeq<T> takeRight(int n);
+
+    @Override
+    IndexedSeq<T> takeWhile(Predicate<? super T> predicate);
+
+    @Override
+    IndexedSeq<T> unit(Iterable<? extends T> iterable);
+
+    @Override
+    <T1, T2> Tuple2<? extends IndexedSeq<T1>, ? extends IndexedSeq<T2>> unzip(Function<? super T, Tuple2<? extends T1, ? extends T2>> unzipper);
+
+    @Override
+    <U> IndexedSeq<Tuple2<T, U>> zip(Iterable<U> that);
+
+    @Override
+    <U> IndexedSeq<Tuple2<T, U>> zipAll(Iterable<U> that, T thisElem, U thatElem);
+
+    @Override
+    IndexedSeq<Tuple2<T, Integer>> zipWithIndex();
+
+}
---
+++ src/main/java/javaslang/collection/LinearSeq.java
@@ -0,0 +1,201 @@
+/*     / \____  _    ______   _____ / \____   ____  _____
+ *    /  \__  \/ \  / \__  \ /  __//  \__  \ /    \/ __  \   Javaslang
+ *  _/  // _\  \  \/  / _\  \\_  \/  // _\  \  /\  \__/  /   Copyright 2014-2015 Daniel Dietrich
+ * /___/ \_____/\____/\_____/____/\___\_____/_/  \_/____/    Licensed under the Apache License, Version 2.0
+ */
+package javaslang.collection;
+
+import javaslang.Kind;
+import javaslang.Tuple2;
+import javaslang.control.Option;
+import javaslang.control.Some;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+/**
+ * Interface for linear sequences.
+ * <p>
+ * Efficient {@code head()}, {@code tail()}, and {@code isEmpty()} methods are characteristic for linear sequences.
+ *
+ * @param <T> component type
+ */
+public interface LinearSeq<T> extends Seq<T> {
+
+        // -- Adjusted return types of Seq methods
+
+    @Override
+    LinearSeq<T> append(T element);
+
+    @Override
+    LinearSeq<T> appendAll(Iterable<? extends T> elements);
+
+    @Override
+    LinearSeq<T> clear();
+
+    @Override
+    LinearSeq<Tuple2<T, T>> cartesianProduct();
+
+    @Override
+    <U> LinearSeq<Tuple2<T, U>> cartesianProduct(Iterable<? extends U> that);
+
+    @Override
+    LinearSeq<? extends LinearSeq<T>> combinations();
+
+    @Override
+    LinearSeq<? extends LinearSeq<T>> combinations(int k);
+
+    @Override
+    LinearSeq<T> distinct();
+
+    @Override
+    <U> LinearSeq<T> distinct(Function<? super T, ? extends U> keyExtractor);
+
+    @Override
+    LinearSeq<T> drop(int n);
+
+    @Override
+    LinearSeq<T> dropRight(int n);
+
+    @Override
+    LinearSeq<T> dropWhile(Predicate<? super T> predicate);
+
+    @Override
+    LinearSeq<T> filter(Predicate<? super T> predicate);
+
+    @Override
+    LinearSeq<Some<T>> filterOption(Predicate<? super T> predicate);
+
+    @Override
+    LinearSeq<T> findAll(Predicate<? super T> predicate);
+
+    @Override
+    <U> LinearSeq<U> flatMap(Function<? super T, ? extends Iterable<? extends U>> mapper);
+
+    @Override
+    <U> LinearSeq<U> flatMapM(Function<? super T, ? extends Kind<? extends IterableKind<?>, ? extends U>> mapper);
+
+    @Override
+    <U> LinearSeq<U> flatten(Function<? super T, ? extends Iterable<? extends U>> f);
+
+    @Override
+    <U> LinearSeq<U> flattenM(Function<? super T, ? extends Kind<? extends IterableKind<?>, ? extends U>> f);
+
+    @Override
+    LinearSeq<? extends LinearSeq<T>> grouped(int size);
+
+    @Override
+    LinearSeq<T> init();
+
+    @Override
+    Option<? extends LinearSeq<T>> initOption();
+
+    @Override
+    LinearSeq<T> insert(int index, T element);
+
+    @Override
+    LinearSeq<T> insertAll(int index, Iterable<? extends T> elements);
+
+    @Override
+    LinearSeq<T> intersperse(T element);
+
+    @Override
+    <U> LinearSeq<U> map(Function<? super T, ? extends U> mapper);
+
+    @Override
+    Tuple2<? extends LinearSeq<T>, ? extends LinearSeq<T>> partition(Predicate<? super T> predicate);
+
+    @Override
+    LinearSeq<T> peek(Consumer<? super T> action);
+
+    @Override
+    LinearSeq<? extends LinearSeq<T>> permutations();
+
+    @Override
+    LinearSeq<T> prepend(T element);
+
+    @Override
+    LinearSeq<T> prependAll(Iterable<? extends T> elements);
+
+    @Override
+    LinearSeq<T> remove(T element);
+
+    @Override
+    LinearSeq<T> removeAll(T element);
+
+    @Override
+    LinearSeq<T> removeAll(Iterable<? extends T> elements);
+
+    @Override
+    LinearSeq<T> replace(T currentElement, T newElement);
+
+    @Override
+    LinearSeq<T> replaceAll(T currentElement, T newElement);
+
+    @Override
+    LinearSeq<T> replaceAll(UnaryOperator<T> operator);
+
+    @Override
+    LinearSeq<T> retainAll(Iterable<? extends T> elements);
+
+    @Override
+    LinearSeq<T> reverse();
+
+    @Override
+    LinearSeq<T> set(int index, T element);
+
+    @Override
+    LinearSeq<? extends LinearSeq<T>> sliding(int size);
+
+    @Override
+    LinearSeq<? extends LinearSeq<T>> sliding(int size, int step);
+
+    @Override
+    LinearSeq<T> sort();
+
+    @Override
+    LinearSeq<T> sort(Comparator<? super T> comparator);
+
+    @Override
+    Tuple2<? extends LinearSeq<T>, ? extends LinearSeq<T>> span(Predicate<? super T> predicate);
+
+    @Override
+    LinearSeq<T> subsequence(int beginIndex);
+
+    @Override
+    LinearSeq<T> subsequence(int beginIndex, int endIndex);
+
+    @Override
+    LinearSeq<T> tail();
+
+    @Override
+    Option<? extends LinearSeq<T>> tailOption();
+
+    @Override
+    LinearSeq<T> take(int n);
+
+    @Override
+    LinearSeq<T> takeRight(int n);
+
+    @Override
+    LinearSeq<T> takeWhile(Predicate<? super T> predicate);
+
+    @Override
+    LinearSeq<T> unit(Iterable<? extends T> iterable);
+
+    @Override
+    <T1, T2> Tuple2<? extends LinearSeq<T1>, ? extends LinearSeq<T2>> unzip(Function<? super T, Tuple2<? extends T1, ? extends T2>> unzipper);
+
+    @Override
+    <U> LinearSeq<Tuple2<T, U>> zip(Iterable<U> that);
+
+    @Override
+    <U> LinearSeq<Tuple2<T, U>> zipAll(Iterable<U> that, T thisElem, U thatElem);
+
+    @Override
+    LinearSeq<Tuple2<T, Integer>> zipWithIndex();
+
+}
