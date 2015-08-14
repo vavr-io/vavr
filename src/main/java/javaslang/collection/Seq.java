18e12a5eddeed4d824c3d8ad78ce3fc11bfed13f
Ruslan Sennov <ruslan.sennov@gmail.com>
2015-08-14 09:35:33 +0300

    javadoc added

--- src/main/java/javaslang/collection/Seq.java
+++ src/main/java/javaslang/collection/Seq.java
@@ -423,8 +423,18 @@ public interface Seq<T> extends Traversable<T>, IntFunction<T> {
      */
     Tuple2<? extends Seq<T>, ? extends Seq<T>> splitAt(int n);
 
+    /**
+     * Splits a sequence at the first element which satisfies the Predicate, e.g. Tuple(init, element+tail).
+     * @param predicate An predicate
+     * @return A Tuple containing divided sequences
+     */
     Tuple2<? extends Seq<T>, ? extends Seq<T>> splitAt(Predicate<? super T> predicate);
 
+    /**
+     * Splits a sequence at the first element which satisfies the Predicate, e.g. Tuple(init+element, tail).
+     * @param predicate An predicate
+     * @return A Tuple containing divided sequences
+     */
     Tuple2<? extends Seq<T>, ? extends Seq<T>> splitAtInclusive(Predicate<? super T> predicate);
 
     /**
