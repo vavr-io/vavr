e4a9e4d6dbbbf7c0e2d320f009e19daad587189d
Daniel Dietrich <daniel.dietrich@gmx.net>
2015-09-21 17:21:12 +0200

    Added javaslang.concurrent package-info

---
+++ src/main/java/javaslang/concurrent/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains basic building blocks for concurrent and parallel programming, namely
+ * {@linkplain javaslang.concurrent.Future} and {@linkplain javaslang.concurrent.Promise}.
+ *
+ * @since 2.0.0
+ */
+package javaslang.concurrent;
