e137232416b83ceece2b11c7a612f362fe319937
Victor Buldakov <vbuldakov@yandex-team.ru>
2015-11-21 01:30:01 +0300

    fix wrong code

--- src/main/java/javaslang/collection/TreeMap.java
+++ src/main/java/javaslang/collection/TreeMap.java
@@ -144,8 +144,6 @@ public final class TreeMap<K, V> implements SortedMap<K, V>, Iterable<Tuple2<K, 
      */
     public static <K extends Comparable<? super K>, V> TreeMap<K, V> of(Comparator<? super K> keyComparator, K key, V value) {
         Objects.requireNonNull(keyComparator, "keyComparator is null");
-        Objects.requireNonNull(key, "key is null");
-        Objects.requireNonNull(value, "value is null");
         return TreeMap.<K, V>empty(keyComparator).put(key, value);
     }
 
