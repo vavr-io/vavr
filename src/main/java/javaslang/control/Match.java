krzysztofik/javaslang

add pattern matching consumer and runnable to MatchFunction

src/main/java/javaslang/control/Match.java
@@ -182,13 +182,29 @@ public interface Match<R> extends Function<Object, R> {
 
             public <R> When.Then<R> then(Supplier<? extends R> supplier) {
                 Objects.requireNonNull(supplier, "supplier is null");
-                return then(ignored -> supplier.get());
+                return then(ignored -> {return supplier.get();});
             }
 
             public <R> When.Then<R> thenThrow(Supplier<? extends RuntimeException> supplier) {
                 Objects.requireNonNull(supplier, "supplier is null");
-                return then(ignored -> {
+                return then((Function<? super T, ? extends R>) ignored -> {
                     throw supplier.get();
+                });
+            }
+
+            public When.Then<Void> then(Consumer<? super T> action) {
+                Objects.requireNonNull(action, "action is null");
+                return then(value -> {
+                    action.accept(value);
+                    return null;
+                });
+            }
+
+            public When.Then<Void> then(Runnable action) {
+                Objects.requireNonNull(action, "action is null");
+                return then(value -> {
+                    action.run();
+                    return null;
                 });
             }
         }
@@ -214,13 +230,29 @@ public interface Match<R> extends Function<Object, R> {
 
             public Then<R> then(Supplier<? extends R> supplier) {
                 Objects.requireNonNull(supplier, "supplier is null");
-                return then(ignored -> supplier.get());
+                return then(ignored -> {return supplier.get();});
             }
 
             public Then<R> thenThrow(Supplier<? extends RuntimeException> supplier) {
                 Objects.requireNonNull(supplier, "supplier is null");
-                return then(ignored -> {
+                return then((Function<? super T, ? extends R>) ignored -> {
                     throw supplier.get();
+                });
+            }
+
+            public When.Then<R> then(Consumer<? super T> action) {
+                Objects.requireNonNull(action, "action is null");
+                return then(value -> {
+                    action.accept(value);
+                    return null;
+                });
+            }
+
+            public When.Then<R> then(Runnable action) {
+                Objects.requireNonNull(action, "action is null");
+                return then(value -> {
+                    action.run();
+                    return null;
                 });
             }
 
@@ -316,6 +348,22 @@ public interface Match<R> extends Function<Object, R> {
                     Objects.requireNonNull(supplier, "supplier is null");
                     return new Otherwise<>(ignored -> {
                         throw supplier.get();
+                    }, cases);
+                }
+
+                public Otherwise<R> otherwise(Consumer<? super Object> action) {
+                    Objects.requireNonNull(action, "action is null");
+                    return new Otherwise<>(value -> {
+                        action.accept(value);
+                        return null;
+                    }, cases);
+                }
+
+                public Otherwise<R> otherwise(Runnable action) {
+                    Objects.requireNonNull(action, "action is null");
+                    return new Otherwise<>(value -> {
+                        action.run();
+                        return null;
                     }, cases);
                 }
             }

src/test/java/javaslang/control/MatchFunctionTest.java
@@ -8,10 +8,12 @@ package javaslang.control;
 import javaslang.Function1;
 import org.junit.Test;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.fail;
 
 public class MatchFunctionTest {
@@ -608,6 +610,89 @@ public class MatchFunctionTest {
             fail("nothing thrown");
         } catch (RuntimeException x) {
             // ok
+        }
+    }
+
+    @Test
+    public void shouldConsumeDefaultCaseWhenNoMatchFound() {
+        //given
+        final IntegerConsumer integerConsumer = new IntegerConsumer();
+        //when
+        Match.whenIs(1).then(integerConsumer)
+             .otherwise(i -> {integerConsumer.accept(2);})
+             .apply(2);
+        //then
+        assertThat(integerConsumer.value).isEqualTo(2);
+    }
+
+    @Test
+    public void shouldConsumeFirstMatchingCase() {
+        //given
+        final IntegerConsumer integerConsumer = new IntegerConsumer();
+        //when
+        Match.whenIs(2).then(integerConsumer)
+             .when(String::isEmpty).then(i -> {integerConsumer.accept(-1);})
+             .otherwise(i -> {integerConsumer.accept(3);})
+             .apply("");
+
+        //then
+        assertThat(integerConsumer.value).isEqualTo(-1);
+    }
+
+    @Test
+    public void shouldNotConsumeWhenNoMatchFoundAnoNoDefaultCase() {
+        //given
+        final IntegerConsumer integerConsumer = new IntegerConsumer();
+        final Function<Object, Void> match = Match.whenIs(1).then(integerConsumer)
+                                                  .whenIs(2).then(integerConsumer)
+                                                  .whenType(String.class).then(i -> {integerConsumer.accept(3);});
+        //expect
+        assertThatThrownBy(() -> match.apply(10)).isInstanceOf(MatchError.class);
+        assertThat(integerConsumer.value).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldAllowToDefineRunnableAsDefaultCase() {
+        //given
+        final SimpleRunnable simpleRunnable = new SimpleRunnable();
+
+        //when
+        Match.whenIs(1).then(simpleRunnable)
+             .otherwise(simpleRunnable)
+             .apply(2);
+
+        //then
+        assertThat(simpleRunnable.executed).isEqualTo(true);
+    }
+
+    @Test
+    public void shouldAllowToDefineRunnableAsMatchingAction() {
+        //given
+        final SimpleRunnable simpleRunnable = new SimpleRunnable();
+        final Function<Object, Void> match = Match.whenIs(1).then(simpleRunnable)
+                                                  .when(o -> !simpleRunnable.executed)
+                                                  .thenThrow(() -> new IllegalStateException("runnable not consumed"));
+        //expect
+        assertThatThrownBy(() -> match.apply(2))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("runnable not consumed");
+    }
+
+    static private class IntegerConsumer implements Consumer<Integer> {
+        public int value;
+
+        @Override
+        public void accept(Integer i) {
+            this.value = i;
+        }
+    }
+
+    static private class SimpleRunnable implements Runnable {
+        public boolean executed;
+
+        @Override
+        public void run() {
+            this.executed=true;
         }
     }
 }

vavr-io/vavr

src/main/java/javaslang/control/Match.java
@@ -191,6 +191,22 @@ public interface Match<R> extends Function<Object, R> {
                     throw supplier.get();
                 });
             }
+
+            public When.Then<Void> thenRun(Consumer<? super T> action) {
+                Objects.requireNonNull(action, "action is null");
+                return then(value -> {
+                    action.accept(value);
+                    return null;
+                });
+            }
+
+            public When.Then<Void> thenRun(Runnable action) {
+                Objects.requireNonNull(action, "action is null");
+                return then(value -> {
+                    action.run();
+                    return null;
+                });
+            }
         }
 
         final class When<T, R> {
@@ -221,6 +237,22 @@ public interface Match<R> extends Function<Object, R> {
                 Objects.requireNonNull(supplier, "supplier is null");
                 return then(ignored -> {
                     throw supplier.get();
+                });
+            }
+
+            public When.Then<R> thenRun(Consumer<? super T> action) {
+                Objects.requireNonNull(action, "action is null");
+                return then(value -> {
+                    action.accept(value);
+                    return null;
+                });
+            }
+
+            public When.Then<R> thenRun(Runnable action) {
+                Objects.requireNonNull(action, "action is null");
+                return then(value -> {
+                    action.run();
+                    return null;
                 });
             }
 
@@ -316,6 +348,22 @@ public interface Match<R> extends Function<Object, R> {
                     Objects.requireNonNull(supplier, "supplier is null");
                     return new Otherwise<>(ignored -> {
                         throw supplier.get();
+                    }, cases);
+                }
+
+                public Otherwise<R> otherwiseRun(Consumer<? super Object> action) {
+                    Objects.requireNonNull(action, "action is null");
+                    return new Otherwise<>(value -> {
+                        action.accept(value);
+                        return null;
+                    }, cases);
+                }
+
+                public Otherwise<R> otherwiseRun(Runnable action) {
+                    Objects.requireNonNull(action, "action is null");
+                    return new Otherwise<>(value -> {
+                        action.run();
+                        return null;
                     }, cases);
                 }
             }

src/test/java/javaslang/control/MatchFunctionTest.java
@@ -8,10 +8,12 @@ package javaslang.control;
 import javaslang.Function1;
 import org.junit.Test;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.fail;
 
 public class MatchFunctionTest {
@@ -608,6 +610,90 @@ public class MatchFunctionTest {
             fail("nothing thrown");
         } catch (RuntimeException x) {
             // ok
+        }
+    }
+
+    @Test
+    public void shouldConsumeDefaultCaseWhenNoMatchFound() {
+        //given
+        final IntegerConsumer integerConsumer = new IntegerConsumer();
+        //when
+        Match.whenIs(1).thenRun(integerConsumer)
+             .otherwiseRun(i -> {integerConsumer.accept(2);})
+             .apply(2);
+        //then
+        assertThat(integerConsumer.value).isEqualTo(2);
+    }
+
+    @Test
+    public void shouldConsumeFirstMatchingCase() {
+        //given
+        final IntegerConsumer integerConsumer = new IntegerConsumer();
+        //when
+        Match.whenIs(2).thenRun(integerConsumer)
+             .when(String::isEmpty).thenRun(i -> {integerConsumer.accept(-1);})
+             .otherwiseRun(i -> {integerConsumer.accept(3);})
+             .apply("");
+
+        //then
+        assertThat(integerConsumer.value).isEqualTo(-1);
+    }
+
+    @Test
+    public void shouldNotConsumeWhenNoMatchFoundAnoNoDefaultCase() {
+        //given
+        final IntegerConsumer integerConsumer = new IntegerConsumer();
+        final Function<Object, Void> match = Match.whenIs(1).thenRun(integerConsumer)
+                                                  .whenIs(2).thenRun(integerConsumer)
+                                                  .whenType(String.class).thenRun(i -> {integerConsumer.accept(3);});
+        //expect
+        assertThatThrownBy(() -> match.apply(10)).isInstanceOf(MatchError.class);
+        assertThat(integerConsumer.value).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldAllowToDefineRunnableAsDefaultCase() {
+        //given
+        final SimpleRunnable simpleRunnable = new SimpleRunnable();
+
+        //when
+        Match.whenIs(1).thenRun(simpleRunnable)
+             .otherwiseRun(simpleRunnable)
+             .apply(2);
+
+        //then
+        assertThat(simpleRunnable.executed).isEqualTo(true);
+    }
+
+    @Test
+    public void shouldAllowToDefineRunnableAsMatchingAction() {
+        //given
+        final SimpleRunnable simpleRunnable = new SimpleRunnable();
+        final Function<Object, Void> match = Match.whenIs(1).thenRun(simpleRunnable)
+                                                  .whenIs(2).thenRun(simpleRunnable)
+                                                  .when(o -> !simpleRunnable.executed)
+                                                  .thenThrow(() -> new IllegalStateException("runnable not consumed"));
+        //expect
+        assertThatThrownBy(() -> match.apply(3))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("runnable not consumed");
+    }
+
+    static private class IntegerConsumer implements Consumer<Integer> {
+        public int value;
+
+        @Override
+        public void accept(Integer i) {
+            this.value = i;
+        }
+    }
+
+    static private class SimpleRunnable implements Runnable {
+        public boolean executed;
+
+        @Override
+        public void run() {
+            this.executed=true;
         }
     }
 }