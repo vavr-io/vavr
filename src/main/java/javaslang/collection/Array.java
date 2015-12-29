9684392066c4c0191cde24fd4ec1071860fe8bf2
Simone Carriero <simonecarriero@gmail.com>
2015-12-29 18:55:42 +0100

    Remove useless cast in Array tabulate

--- src/main/java/javaslang/collection/Array.java
+++ src/main/java/javaslang/collection/Array.java
@@ -216,8 +216,7 @@ public final class Array<T> implements IndexedSeq<T>, Serializable {
     public static <T> Array<T> tabulate(int n, Function<? super Integer, ? extends T> f) {
         Objects.requireNonNull(f, "f is null");
         int nOrZero = java.lang.Math.max(n, 0);
-        @SuppressWarnings("unchecked")
-        T[] elements = (T[]) new Object[nOrZero];
+        Object[] elements = new Object[nOrZero];
         for (int i = 0; i < n; i++) {
             elements[i] = f.apply(i);
         }
