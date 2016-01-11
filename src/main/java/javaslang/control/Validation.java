84210190a406b4f02fd243857ddadd5e41027e82
Eric Nelson <enelson@ancestry.com>
2016-01-11 14:35:09 -0700

    adding more javadocs to Validation class

--- src/main/java/javaslang/control/Validation.java
+++ src/main/java/javaslang/control/Validation.java
@@ -27,13 +27,26 @@ import java.util.function.Supplier;
  * </p>
  *
  * <pre>
- * {@code Validation} construction:
  * <code>
- * Validation.Valid:
+ * <b>Validation construction:</b>
+ *
+ * <i>Valid:</i>
  * Validation&lt;String,Integer&gt; valid = Validation.valid(5);
  *
- * Validation.Invalid:
- * Validation&lt;String,Integer&gt; invalid = Validation.invalid(List.of("error1","error2"));
+ * <i>Invalid:</i>
+ * Validation&lt;List&lt;String&gt;,Integer&gt; invalid = Validation.invalid(List.of("error1","error2"));
+ *
+ * <b>Validation combination:</b>
+ *
+ * Validation&lt;String,String&gt; valid1 = Validation.valid("John");
+ * Validation&lt;String,Integer&gt; valid2 = Validation.valid(5);
+ * Validation&lt;String,Option&lt;String&gt;&gt; valid3 = Validation.valid(Option.of("123 Fake St."));
+ * Function3&lt;String,Integer,Option&lt;String&gt;,Person&gt; f = ...;
+ *
+ * Validation&lt;List&lt;String&gt;,String&gt; result = valid1.combine(valid2).ap((name,age) -&gt; "Name: "+name+" Age: "+age);
+ * Validation&lt;List&lt;String&gt;,Person&gt; result2 = valid1.combine(valid2).combine(valid3).ap(f);
+ *
+ *
  * </code>
  * </pre>
  *
@@ -41,36 +54,96 @@ import java.util.function.Supplier;
  * @param <T> value type in the case of valid
  * @author Eric Nelson
  * @since 2.0.0
- * @see <a href="http://eed3si9n.com/learning-scalaz/Validation.html">Validation</a>
+ * @see <a href="https://github.com/scalaz/scalaz/blob/series/7.3.x/core/src/main/scala/scalaz/Validation.scala">Validation</a>
  */
 public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicative<Validation<?,?>, E, T> {
 
+    /**
+     * Creates a {@link Valid} that contains the given {@code value}.
+     *
+     * @param <E> type of the error
+     * @param <T> type of the given {@code value}
+     * @param value A value
+     * @return {@code Valid(value)}
+     * @throws NullPointerException if value is null
+     */
     static <E,T> Validation<E,T> valid(T value) {
         Objects.requireNonNull(value, "value is null");
         return new Valid<>(value);
     }
 
+    /**
+     * Creates a {@link Valid} that contains the result of the given {@code supplier}.
+     *
+     * @param <E> type of the error
+     * @param <T> type of the result of the given {@code supplier}
+     * @param supplier A supplier for the value
+     * @return {@code Valid(value)}
+     * @throws NullPointerException if supplier is null
+     */
     static <E,T> Validation<E,T> valid(Supplier<? extends T> supplier) {
         Objects.requireNonNull(supplier, "supplier is null");
         return new Valid<>(supplier.get());
     }
 
+    /**
+     * Creates an {@link Invalid} that contains the given {@code error}.
+     *
+     * @param <E> type of the given {@code error}
+     * @param <T> type of the value
+     * @param error An error
+     * @return {@code Invalid(error)}
+     * @throws NullPointerException if error is null
+     */
     static <E,T> Validation<E,T> invalid(E error) {
         Objects.requireNonNull(error, "error is null");
         return new Invalid<>(error);
     }
 
+    /**
+     * Creates an {@link Invalid} that contains the result of the given {@code supplier}.
+     *
+     * @param <E> type of the result of the given {@code supplier}
+     * @param <T> type of the value
+     * @param supplier A supplier for the error
+     * @return {@code Invalid(error)}
+     * @throws NullPointerException if supplier is null
+     */
     static <E,T> Validation<E,T> invalid(Supplier<? extends E> supplier) {
         Objects.requireNonNull(supplier, "supplier is null");
         return new Invalid<>(supplier.get());
     }
 
+    /**
+     * Combines two {@code Validation}s into a {@link Builder}.
+     *
+     * @param <E> type of error
+     * @param <T1> type of first valid value
+     * @param <T2> type of second valid value
+     * @param validation1 first validation
+     * @param validation2 second validation
+     * @return an instance of Builder&lt;E,T1,T2&gt;
+     * @throws NullPointerException if validation1 or validation2 is null
+     */
     static <E,T1,T2> Builder<E,T1,T2> map2(Validation<E,T1> validation1, Validation<E,T2> validation2) {
         Objects.requireNonNull(validation1, "validation1 is null");
         Objects.requireNonNull(validation2, "validation2 is null");
         return new Builder<>(validation1, validation2);
     }
 
+    /**
+     * Combines three {@code Validation}s into a {@link Builder3}.
+     *
+     * @param <E> type of error
+     * @param <T1> type of first valid value
+     * @param <T2> type of second valid value
+     * @param <T3> type of third valid value
+     * @param validation1 first validation
+     * @param validation2 second validation
+     * @param validation3 third validation
+     * @return an instance of Builder3&lt;E,T1,T2,T3&gt;
+     * @throws NullPointerException if validation1, validation2 or validation3 is null
+     */
     static <E,T1,T2,T3> Builder3<E,T1,T2,T3> map3(Validation<E,T1> validation1, Validation<E,T2> validation2, Validation<E,T3> validation3) {
         Objects.requireNonNull(validation1, "validation1 is null");
         Objects.requireNonNull(validation2, "validation2 is null");
@@ -78,6 +151,21 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         return new Builder3<>(validation1, validation2, validation3);
     }
 
+    /**
+     * Combines four {@code Validation}s into a {@link Builder4}.
+     *
+     * @param <E> type of error
+     * @param <T1> type of first valid value
+     * @param <T2> type of second valid value
+     * @param <T3> type of third valid value
+     * @param <T4> type of fourth valid value
+     * @param validation1 first validation
+     * @param validation2 second validation
+     * @param validation3 third validation
+     * @param validation4 fourth validation
+     * @return an instance of Builder3&lt;E,T1,T2,T3,T4&gt;
+     * @throws NullPointerException if validation1, validation2, validation3 or validation4 is null
+     */
     static <E,T1,T2,T3,T4> Builder4<E,T1,T2,T3,T4> map4(Validation<E,T1> validation1, Validation<E,T2> validation2, Validation<E,T3> validation3, Validation<E,T4> validation4) {
         Objects.requireNonNull(validation1, "validation1 is null");
         Objects.requireNonNull(validation2, "validation2 is null");
@@ -86,6 +174,23 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         return new Builder4<>(validation1, validation2, validation3, validation4);
     }
 
+    /**
+     * Combines five {@code Validation}s into a {@link Builder5}.
+     *
+     * @param <E> type of error
+     * @param <T1> type of first valid value
+     * @param <T2> type of second valid value
+     * @param <T3> type of third valid value
+     * @param <T4> type of fourth valid value
+     * @param <T5> type of fifth valid value
+     * @param validation1 first validation
+     * @param validation2 second validation
+     * @param validation3 third validation
+     * @param validation4 fourth validation
+     * @param validation5 fifth validation
+     * @return an instance of Builder3&lt;E,T1,T2,T3,T4,T5&gt;
+     * @throws NullPointerException if validation1, validation2, validation3, validation4 or validation5 is null
+     */
     static <E,T1,T2,T3,T4,T5> Builder5<E,T1,T2,T3,T4,T5> map5(Validation<E,T1> validation1, Validation<E,T2> validation2, Validation<E,T3> validation3, Validation<E,T4> validation4, Validation<E,T5> validation5) {
         Objects.requireNonNull(validation1, "validation1 is null");
         Objects.requireNonNull(validation2, "validation2 is null");
@@ -95,6 +200,25 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         return new Builder5<>(validation1, validation2, validation3, validation4, validation5);
     }
 
+    /**
+     * Combines six {@code Validation}s into a {@link Builder6}.
+     *
+     * @param <E> type of error
+     * @param <T1> type of first valid value
+     * @param <T2> type of second valid value
+     * @param <T3> type of third valid value
+     * @param <T4> type of fourth valid value
+     * @param <T5> type of fifth valid value
+     * @param <T6> type of sixth valid value
+     * @param validation1 first validation
+     * @param validation2 second validation
+     * @param validation3 third validation
+     * @param validation4 fourth validation
+     * @param validation5 fifth validation
+     * @param validation6 sixth validation
+     * @return an instance of Builder3&lt;E,T1,T2,T3,T4,T5,T6&gt;
+     * @throws NullPointerException if validation1, validation2, validation3, validation4, validation5 or validation6 is null
+     */
     static <E,T1,T2,T3,T4,T5,T6> Builder6<E,T1,T2,T3,T4,T5,T6> map6(Validation<E,T1> validation1, Validation<E,T2> validation2, Validation<E,T3> validation3, Validation<E,T4> validation4, Validation<E,T5> validation5, Validation<E,T6> validation6) {
         Objects.requireNonNull(validation1, "validation1 is null");
         Objects.requireNonNull(validation2, "validation2 is null");
@@ -105,6 +229,27 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         return new Builder6<>(validation1, validation2, validation3, validation4, validation5, validation6);
     }
 
+    /**
+     * Combines seven {@code Validation}s into a {@link Builder7}.
+     *
+     * @param <E> type of error
+     * @param <T1> type of first valid value
+     * @param <T2> type of second valid value
+     * @param <T3> type of third valid value
+     * @param <T4> type of fourth valid value
+     * @param <T5> type of fifth valid value
+     * @param <T6> type of sixth valid value
+     * @param <T7> type of seventh valid value
+     * @param validation1 first validation
+     * @param validation2 second validation
+     * @param validation3 third validation
+     * @param validation4 fourth validation
+     * @param validation5 fifth validation
+     * @param validation6 sixth validation
+     * @param validation7 seventh validation
+     * @return an instance of Builder3&lt;E,T1,T2,T3,T4,T5,T6,T7&gt;
+     * @throws NullPointerException if validation1, validation2, validation3, validation4, validation5, validation6 or validation7 is null
+     */
     static <E,T1,T2,T3,T4,T5,T6,T7> Builder7<E,T1,T2,T3,T4,T5,T6,T7> map7(Validation<E,T1> validation1, Validation<E,T2> validation2, Validation<E,T3> validation3, Validation<E,T4> validation4, Validation<E,T5> validation5, Validation<E,T6> validation6, Validation<E,T7> validation7) {
         Objects.requireNonNull(validation1, "validation1 is null");
         Objects.requireNonNull(validation2, "validation2 is null");
@@ -116,6 +261,29 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         return new Builder7<>(validation1, validation2, validation3, validation4, validation5, validation6, validation7);
     }
 
+    /**
+     * Combines eight {@code Validation}s into a {@link Builder8}.
+     *
+     * @param <E> type of error
+     * @param <T1> type of first valid value
+     * @param <T2> type of second valid value
+     * @param <T3> type of third valid value
+     * @param <T4> type of fourth valid value
+     * @param <T5> type of fifth valid value
+     * @param <T6> type of sixth valid value
+     * @param <T7> type of seventh valid value
+     * @param <T8> type of eighth valid value
+     * @param validation1 first validation
+     * @param validation2 second validation
+     * @param validation3 third validation
+     * @param validation4 fourth validation
+     * @param validation5 fifth validation
+     * @param validation6 sixth validation
+     * @param validation7 seventh validation
+     * @param validation8 eigth validation
+     * @return an instance of Builder3&lt;E,T1,T2,T3,T4,T5,T6,T7,T8&gt;
+     * @throws NullPointerException if validation1, validation2, validation3, validation4, validation5, validation6, validation7 or validation8 is null
+     */
     static <E,T1,T2,T3,T4,T5,T6,T7,T8> Builder8<E,T1,T2,T3,T4,T5,T6,T7,T8> map8(Validation<E,T1> validation1, Validation<E,T2> validation2, Validation<E,T3> validation3, Validation<E,T4> validation4, Validation<E,T5> validation5, Validation<E,T6> validation6, Validation<E,T7> validation7, Validation<E,T8> validation8) {
         Objects.requireNonNull(validation1, "validation1 is null");
         Objects.requireNonNull(validation2, "validation2 is null");
@@ -128,12 +296,34 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         return new Builder8<>(validation1, validation2, validation3, validation4, validation5, validation6, validation7, validation8);
     }
 
+    /**
+     * Check whether this is of type {@code Valid}
+     *
+     * @return true if is a Valid, false if is an Invalid
+     */
     boolean isValid();
 
+    /**
+     * Check whether this is of type {@code Invalid}
+     *
+     * @return true if is an Invalid, false if is a Valid
+     */
     boolean isInvalid();
 
+    /**
+     * Gets the value of this Validation if is a Valid or throws if this is an Invalid
+     *
+     * @return The value of this Validation
+     * @throws RuntimeException if this is an Invalid
+     */
     T get();
 
+    /**
+     * Gets the error of this Validation if is an Invalid or throws if this is a Valid
+     *
+     * @return The error of this Invalid
+     * @throws RuntimeException if this is a Valid
+     */
     E getError();
 
     @Override
@@ -145,12 +335,37 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
     @Override
     String toString();
 
-    default void forEach(Consumer<? super T> f) {
-        Objects.requireNonNull(f, "function f is null");
+    /**
+     * Performs the given action for the value contained in {@code Valid}, or do nothing
+     * if this is an Invalid.
+     *
+     * @param action the action to be performed on the contained value
+     * @throws NullPointerException if action is null
+     */
+    default void forEach(Consumer<? super T> action) {
+        Objects.requireNonNull(action, "function f is null");
         if(isValid())
-            f.accept(get());
+            action.accept(get());
     }
 
+    /**
+     * Performs the action in fInvalid on error if this is an Invalid, or fValid on value if
+     * this is a Valid. Returns an object of type U.
+     *
+     * <p>
+     * <code>
+     * For example:<br>
+     * Validation&lt;List&lt;String&gt;,String&gt; valid = ...;<br>
+     * Integer i = valid.fold(List::length, String::length);
+     * </code>
+     * </p>
+     *
+     * @param <U> the fold result type
+     * @param fInvalid the invalid fold operation
+     * @param fValid the valid fold operation
+     * @return an instance of type U
+     * @throws NullPointerException if fInvalid or fValid is null
+     */
     default <U> U fold(Function<? super E,? extends U> fInvalid, Function<? super T,? extends U> fValid) {
         Objects.requireNonNull(fInvalid, "function fInvalid null");
         Objects.requireNonNull(fValid, "function fValid null");
@@ -163,6 +378,12 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         }
     }
 
+    /**
+     * Flip the valid/invalid values for this Validation. If this is a Valid&lt;E,T&gt;, returns Invalid&lt;T,E&gt;.
+     * Or if this is an Invalid&lt;E,T&gt;, return a Valid&lt;T,E&gt;.
+     *
+     * @return a flipped instance of Validation
+     */
     default Validation<T,E> swap() {
         if(isInvalid()) {
             E error = this.getError();
@@ -184,6 +405,20 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         }
     }
 
+    /**
+     * Whereas map only performs a mapping on a valid Validation, and leftMap performs a mapping on an invalid
+     * Validation, bimap allows you to provide mapping actions for both, and will give you the result based
+     * on what type of Validation this is. Without this, you would have to do something like:
+     *
+     * validation.map(...).leftMap(...);
+     *
+     * @param <U> type of the mapping result if this is an invalid
+     * @param <R> type of the mapping result if this is a valid
+     * @param invalidMapper the invalid mapping operation
+     * @param validMapper the valid mapping operation
+     * @return an instance of Validation&lt;U,R&gt;
+     * @throws NullPointerException if invalidMapper or validMapper is null
+     */
     default <U,R> Validation<U,R> bimap(Function<? super E,? extends U> invalidMapper, Function<? super T,? extends R> validMapper) {
         Objects.requireNonNull(invalidMapper, "function invalidMapper is null");
         Objects.requireNonNull(validMapper, "function validMapper is null");
@@ -196,6 +431,15 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         }
     }
 
+    /**
+     * Applies a function f to the error of this Validation if this is an Invalid. Otherwise does nothing
+     * if this is a Valid.
+     *
+     * @param <U> type of the error resulting from the mapping
+     * @param f a function that maps the error in this Invalid
+     * @return an instance of Validation&lt;U,T&gt;
+     * @throws NullPointerException if mapping operation f is null
+     */
     default <U> Validation<U,T> leftMap(Function<? super E,? extends U> f) {
         Objects.requireNonNull(f, "function f is null");
         if(isInvalid()) {
@@ -232,17 +476,32 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
         }
     }
 
+    /**
+     * Combines two {@code Validation}s to form a {@link Builder}, which can then be used to perform further
+     * combines, or apply a function to it in order to transform the {@link Builder} into a {@code Validation}.
+     *
+     * @param <U> type of the value contained in validation
+     * @param validation the validation object to combine this with
+     * @return an instance of Builder
+     */
     default <U> Builder<E,T,U> combine(Validation<E,U> validation) {
         return new Builder<>(this, validation);
     }
 
 
+    /**
+     * A valid Validation
+     *
+     * @param <E> type of the error of this Validation
+     * @param <T> type of the value of this Validation
+     */
     final class Valid<E,T> implements Validation<E,T> {
 
         private final T value;
 
         /**
          * Construct a {@code Valid}
+         *
          * @param value The value of this success
          */
         private Valid(T value) {
@@ -286,12 +545,19 @@ public interface Validation<E,T> extends Kind2<Validation<?,?>, E, T>, Applicati
 
     }
 
+    /**
+     * An invalid Validation
+     *
+     * @param <E> type of the error of this Validation
+     * @param <T> type of the value of this Validation
+     */
     final class Invalid<E,T> implements Validation<E,T> {
 
         private final E error;
 
         /**
          * Construct an {@code Invalid}
+         *
          * @param error The value of this error
          */
         private Invalid(E error) {
