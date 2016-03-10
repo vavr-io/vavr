326d6012424363f1f3bbb193366ea0ce9f861883
Daniel Dietrich <daniel.dietrich@gmx.net>
2016-03-10 09:51:23 +0100

    Refactored the annotation code generator. Compiles now.

--- javaslang-match/src/main/java/javaslang/match/annotation/Patterns.java
+++ javaslang-match/src/main/java/javaslang/match/annotation/Patterns.java
@@ -3,7 +3,7 @@
  *  _/  /  /\  \  \/  /  /\  \\__\\  \  //  /\  \ /\\/ \ /__\ \   Copyright 2014-2016 Javaslang, http://javaslang.io
  * /___/\_/  \_/\____/\_/  \_/\__\/__/\__\_/  \_//  \__/\_____/   Licensed under the Apache License, Version 2.0
  */
-package javaslang.match;
+package javaslang.match.annotation;
 
 import javax.annotation.processing.Messager;
 import javax.lang.model.element.TypeElement;
@@ -12,6 +12,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Structural pattern matching annotation for pattern declarations.
+ *
+ * @author Daniel Dietrich
+ * @since 2.0.0
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 public @interface Patterns {
@@ -21,7 +27,7 @@ public @interface Patterns {
      */
     class Checker extends BaseChecker<Checker, TypeElement> {
 
-        static boolean isValid(TypeElement typeElement, Messager messager) {
+        public static boolean isValid(TypeElement typeElement, Messager messager) {
             final Checker typeChecker = new Checker(typeElement, messager);
             return typeChecker.ensure(e -> !e.isNested(), () -> "Patterns need to be defined in top-level classes.");
         }
--- javaslang-match/src/main/java/javaslang/match/annotation/Unapply.java
+++ javaslang-match/src/main/java/javaslang/match/annotation/Unapply.java
@@ -3,10 +3,11 @@
  *  _/  /  /\  \  \/  /  /\  \\__\\  \  //  /\  \ /\\/ \ /__\ \   Copyright 2014-2016 Javaslang, http://javaslang.io
  * /___/\_/  \_/\____/\_/  \_/\__\/__/\__\_/  \_//  \__/\_____/   Licensed under the Apache License, Version 2.0
  */
-package javaslang.match;
+package javaslang.match.annotation;
 
 import javax.annotation.processing.Messager;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.type.TypeKind;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -14,6 +15,12 @@ import java.lang.annotation.Target;
 
 import static javax.lang.model.element.Modifier.*;
 
+/**
+ * Structural pattern matching annotation for unapply methods.
+ *
+ * @author Daniel Dietrich
+ * @since 2.0.0
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.CLASS)
 public @interface Unapply {
@@ -23,13 +30,13 @@ public @interface Unapply {
      */
     class Checker extends BaseChecker<Checker, ExecutableElement> {
 
-        static boolean isValid(ExecutableElement executableElement, Messager messager) {
+        public static boolean isValid(ExecutableElement executableElement, Messager messager) {
             final Checker methodChecker = new Checker(executableElement, messager);
-            return methodChecker.ensure(Checker::isMethod, () -> "Annotation " + Unapply.class.getName() + " only allowed for methods.") &&
-                    methodChecker.ensure(Checker::doesNotThrow, () -> "@" + "Unapply method should not throw (checked) exceptions.") &&
+            return methodChecker.ensure(Checker::doesNotThrow, () -> "@" + "Unapply method should not throw (checked) exceptions.") &&
                     methodChecker.ensure(e -> !e.elem.isDefault(), () -> "@" + "Unapply method needs to be declared in a class, not an interface.") &&
                     methodChecker.ensure(e -> !e.elem.isVarArgs(), () -> "@" + "Unapply method has varargs.") &&
                     methodChecker.ensure(e -> e.elem.getParameters().size() == 1, () -> "Unapply method must have exactly one parameter of the object to be deconstructed.") &&
+                    // TODO: methodChecker.ensure(e -> e.elem.getParameters().get(0).asType().getKind() != TypeKind.DECLARED, () -> "Unapply method parameter must be a declared type.") &&
                     methodChecker.ensure(e -> e.elem.getReturnType().toString().startsWith("javaslang.Tuple"), () -> "Return type of unapply method must be a Tuple.") &&
                     methodChecker.ensure(e -> !e.elem.getReturnType().toString().endsWith("Tuple"), () -> "Return type is no Tuple implementation.") &&
                     methodChecker.ensure(e -> e.hasAll(STATIC), () -> "Unapply method needs to be static.") &&
@@ -38,11 +45,6 @@ public @interface Unapply {
 
         private Checker(ExecutableElement elem, Messager messager) {
             super(elem, messager);
-        }
-
-        boolean isMethod() {
-            final String name = elem.getSimpleName().toString();
-            return !name.isEmpty() && !"<init>".equals(name) && !"<clinit>".equals(name);
         }
 
         boolean doesNotThrow() {
