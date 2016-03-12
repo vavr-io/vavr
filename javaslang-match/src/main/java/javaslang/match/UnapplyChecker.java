5dbf45b0a8be63590c7fc709ec246ad37eda2ab2
Daniel Dietrich <daniel.dietrich@gmx.net>
2016-03-12 11:47:19 +0100

    Decoupled annotation checker from generator

--- javaslang-match/src/main/java/javaslang/match/UnapplyChecker.java
+++ javaslang-match/src/main/java/javaslang/match/UnapplyChecker.java
@@ -3,7 +3,7 @@
  *  _/  /  /\  \  \/  /  /\  \\__\\  \  //  /\  \ /\\/ \ /__\ \   Copyright 2014-2016 Javaslang, http://javaslang.io
  * /___/\_/  \_/\____/\_/  \_/\__\/__/\__\_/  \_//  \__/\_____/   Licensed under the Apache License, Version 2.0
  */
-package javaslang.match.generator;
+package javaslang.match;
 
 import javax.annotation.processing.Messager;
 import javax.lang.model.element.ExecutableElement;
