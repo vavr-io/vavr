b85241c5e5de6d426486a3b6349317d387494a5e
Ruslan Sennov <ruslan.sennov@gmail.com>
2016-08-31 07:37:33 +0300

    GWT unit tests

--- javaslang-gwt/README.md
+++ javaslang-gwt/README.md
@@ -1,10 +1,10 @@
 # GWT support for Javaslang
 
-Tested with 7da4bf36a105faf5811235692bf00e86c958f3de
+Tested with 535f2fa973995f18d190a689127acbe134a42a15
 
 ```
-git checkout 7da4bf
+git checkout 535f2fa9
 ant clean elemental dist-dev
-mvn install:install-file -Dfile=build/lib/gwt-user.jar -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=2.8.0-7da4bf -Dpackaging=jar 
-mvn install:install-file -Dfile=build/lib/gwt-dev.jar -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=2.8.0-7da4bf -Dpackaging=jar 
+mvn install:install-file -Dfile=build/lib/gwt-user.jar -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=2.8.0-535f2fa9 -Dpackaging=jar 
+mvn install:install-file -Dfile=build/lib/gwt-dev.jar -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=2.8.0-535f2fa9 -Dpackaging=jar 
 ```
--- javaslang-gwt/src/test/java/client/GwtTestCollections.java
+++ javaslang-gwt/src/test/java/client/GwtTestCollections.java
@@ -23,43 +23,43 @@ public class GwtTestCollections extends GWTTestCase {
         assertEquals(traversable.count(i -> i != 'b'), 2);
     }
 
-//    public void testCompileArray() {
-//        applyCollection(Array::ofAll);
-//    }
-//
-//    public void testCompileBitSet() {
-//        applyCollection(BitSet::ofAll);
-//    }
-//
-//    public void testCompileCharSeq() {
-//        applyCollection(chars -> CharSeq.of(new String(chars)));
-//    }
-//
-//    public void testCompileHashSet() {
-//        applyCollection(HashSet::ofAll);
-//    }
-//
-//    public void testCompileLinkedHashSet() {
-//        applyCollection(LinkedHashSet::ofAll);
-//    }
-//
+    public void testCompileArray() {
+        applyCollection(Array::ofAll);
+    }
+
+    public void testCompileBitSet() {
+        applyCollection(BitSet::ofAll);
+    }
+
+    public void testCompileCharSeq() {
+        applyCollection(chars -> CharSeq.of(new String(chars)));
+    }
+
+    public void testCompileHashSet() {
+        applyCollection(HashSet::ofAll);
+    }
+
+    public void testCompileLinkedHashSet() {
+        applyCollection(LinkedHashSet::ofAll);
+    }
+
     public void testCompileList() {
         applyCollection(List::ofAll);
     }
-//
-//    public void testCompilePriorityQueue() {
-//        applyCollection(chars -> PriorityQueue.ofAll(List.ofAll(chars)));
-//    }
-//
-//    public void testCompileQueue() {
-//        applyCollection(Queue::ofAll);
-//    }
-//
-//    public void testCompileTreeSet() {
-//        applyCollection(chars -> TreeSet.ofAll(List.ofAll(chars)));
-//    }
-//
-//    public void testCompileVector() {
-//        applyCollection(Vector::ofAll);
-//    }
+
+    public void testCompilePriorityQueue() {
+        applyCollection(chars -> PriorityQueue.ofAll(List.ofAll(chars)));
+    }
+
+    public void testCompileQueue() {
+        applyCollection(Queue::ofAll);
+    }
+
+    public void testCompileTreeSet() {
+        applyCollection(chars -> TreeSet.ofAll(List.ofAll(chars)));
+    }
+
+    public void testCompileVector() {
+        applyCollection(Vector::ofAll);
+    }
 }
