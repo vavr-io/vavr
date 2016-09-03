c1cbfa87e0b313bc9aa352edc91e8afa8171138e
Ruslan Sennov <ruslan.sennov@gmail.com>
2016-09-03 08:39:24 +0300

    apply PR comments

--- javaslang-gwt/README.md
+++ javaslang-gwt/README.md
@@ -1,10 +1,16 @@
 # GWT support for Javaslang
 
-Tested with 535f2fa973995f18d190a689127acbe134a42a15
+Ensure that your `~/.m2/settings.xml` contains the following:
 
-```
-git checkout 535f2fa9
-ant clean elemental dist-dev
-mvn install:install-file -Dfile=build/lib/gwt-user.jar -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=2.8.0-535f2fa9 -Dpackaging=jar 
-mvn install:install-file -Dfile=build/lib/gwt-dev.jar -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=2.8.0-535f2fa9 -Dpackaging=jar 
+```xml
+<repository>
+  <id>snapshots-google</id>
+  <url>https://oss.sonatype.org/content/repositories/google-snapshots</url>
+  <releases>
+    <enabled>false</enabled>
+  </releases>
+  <snapshots>
+    <enabled>true</enabled>
+  </snapshots>
+</repository>
 ```
--- javaslang/src/main/java/javaslang/GwtIncompatible.java
+++ javaslang/src/main/java/javaslang/GwtIncompatible.java
@@ -1,17 +1,7 @@
-/*
- * Copyright 2013 Google Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+/*     / \____  _    _  ____   ______  / \ ____  __    _______
+ *    /  /    \/ \  / \/    \ /  /\__\/  //    \/  \  //  /\__\   JΛVΛSLΛNG
+ *  _/  /  /\  \  \/  /  /\  \\__\\  \  //  /\  \ /\\/ \ /__\ \   Copyright 2014-2016 Javaslang, http://javaslang.io
+ * /___/\_/  \_/\____/\_/  \_/\__\/__/\__\_/  \_//  \__/\_____/   Licensed under the Apache License, Version 2.0
  */
 package javaslang;
 
@@ -27,10 +17,5 @@ import java.lang.annotation.Target;
         ElementType.CONSTRUCTOR, ElementType.FIELD })
 @Documented
 @interface GwtIncompatible {
-    /**
-     * An attribute that can be used to explain why the code is incompatible.
-     * A GwtIncompatible annotation can have any number of attributes; attributes
-     * are for documentation purposes and are ignored by the GWT compiler.
-     */
     String value() default "";
 }
