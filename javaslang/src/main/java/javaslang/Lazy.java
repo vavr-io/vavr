d4d545579f91f13e8df508e6654c457460b96d27
Daniel Dietrich <cafebab3@gmail.com>
2016-09-17 13:29:44 +0200

    further improvement of Lazy.get()

--- javaslang/src/main/java/javaslang/Lazy.java
+++ javaslang/src/main/java/javaslang/Lazy.java
@@ -44,7 +44,9 @@ public final class Lazy<T> implements Value<T>, Supplier<T>, Serializable {
 
     // read http://javarevisited.blogspot.de/2014/05/double-checked-locking-on-singleton-in-java.html
     private transient volatile Supplier<? extends T> supplier;
-    private volatile T value;
+
+    // does not need to be volatile, visibility piggy-backs on volatile read of `supplier`
+    private T value;
 
     // should not be called directly
     private Lazy(Supplier<? extends T> supplier) {
@@ -132,12 +134,13 @@ public final class Lazy<T> implements Value<T>, Supplier<T>, Serializable {
      */
     @Override
     public T get() {
+        // using a local var speeds up the double-check idiom by 25%, see Effective Java, Item 71
         Supplier<? extends T> tmp = supplier;
         if (tmp != null) {
             synchronized (this) {
                 tmp = supplier;
                 if (tmp != null) {
-                    value = supplier.get();
+                    value = tmp.get();
                     supplier = null; // free mem
                 }
             }
