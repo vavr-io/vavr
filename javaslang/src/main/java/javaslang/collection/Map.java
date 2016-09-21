3bea5a62e14450c706603d5282e88d8531558e3d
Ashwin R <ashwinravichandran24@gmail.com>
2016-09-21 09:48:12 +0530

    added unzip to map

--- javaslang/src/main/java/javaslang/collection/Map.java
+++ javaslang/src/main/java/javaslang/collection/Map.java
@@ -521,6 +521,10 @@ public interface Map<K, V> extends Traversable<Tuple2<K, V>>, Function1<K, V> {
         return foldLeft(List.empty(), (acc, entry) -> acc.append(mapper.apply(entry._1, entry._2)));
     }
 
+    default Tuple2<Seq<K>, Seq<V>> unzip() {
+        return this.unzip(Function.identity());
+    }
+
     default <T1, T2> Tuple2<Seq<T1>, Seq<T2>> unzip(BiFunction<? super K, ? super V, Tuple2<? extends T1, ? extends T2>> unzipper) {
         Objects.requireNonNull(unzipper, "unzipper is null");
         return unzip(entry -> unzipper.apply(entry._1, entry._2));
