7948682632dd60348ca2c1adbe31ff409e8d5284
Daniel Dietrich <cafebab3@gmail.com>
2016-11-26 00:57:45 +0100

    Removed javaslang-pure

--- javaslang-pure/src/main/java/javaslang/algebra/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The algebra package contains interfaces used to describe inherent class properties like
- * {@linkplain javaslang.algebra.Applicative}, {@linkplain javaslang.algebra.Monoid} and
- * {@linkplain javaslang.algebra.Monad}.
- *
- * @since 1.1.0
- */
-package javaslang.algebra;
