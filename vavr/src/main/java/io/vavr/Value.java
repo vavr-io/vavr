8d6fcee7a3ca2993b77d2a7663aa9c10104f12e2
Daniel Dietrich <cafebab3@gmail.com>
2017-05-10 13:08:35 +0200

    Future.toTry() preserves existing failure

--- vavr/src/main/java/io/vavr/Value.java
+++ vavr/src/main/java/io/vavr/Value.java
@@ -7,6 +7,7 @@
 package io.vavr;
 
 import io.vavr.collection.*;
+import io.vavr.concurrent.Future;
 import io.vavr.control.Either;
 import io.vavr.control.Option;
 import io.vavr.control.Try;
@@ -1132,6 +1133,8 @@ public interface Value<T> extends Iterable<T> {
     default Try<T> toTry() {
         if (this instanceof Try) {
             return (Try<T>) this;
+        } else if (this instanceof Future) {
+            return ((Future<T>) this).await().getValue().get();
         } else {
             return Try.of(this::get);
         }
