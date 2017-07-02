658367913ce176fc52bd29f1cec3ed63608c9f83
Ruslan Sennov <ruslan.sennov@gmail.com>
2017-07-02 13:55:07 +0300

    *.*.of(KVKVKV) generated tests (#2026)

---
+++ vavr/src-gen/test/java/io/vavr/collection/TreeMapOfEntriesTest.java
@@ -0,0 +1,288 @@
+/*                        __    __  __  __    __  ___
+ *                       \  \  /  /    \  \  /  /  __/
+ *                        \  \/  /  /\  \  \/  /  /
+ *                         \____/__/  \__\____/__/.ɪᴏ
+ * ᶜᵒᵖʸʳᶦᵍʰᵗ ᵇʸ ᵛᵃᵛʳ ⁻ ˡᶦᶜᵉⁿˢᵉᵈ ᵘⁿᵈᵉʳ ᵗʰᵉ ᵃᵖᵃᶜʰᵉ ˡᶦᶜᵉⁿˢᵉ ᵛᵉʳˢᶦᵒⁿ ᵗʷᵒ ᵈᵒᵗ ᶻᵉʳᵒ
+ */
+package io.vavr.collection;
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*\
+   G E N E R A T O R   C R A F T E D
+\*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+
+import static io.vavr.collection.Comparators.naturalComparator;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class TreeMapOfEntriesTest {
+  @Test
+  public void shouldConstructFrom1EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1");
+    assertThat(map.size()).isEqualTo(1);
+    assertThat(map.get(1).get()).isEqualTo("1");
+  }
+
+  @Test
+  public void shouldConstructFrom2EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2");
+    assertThat(map.size()).isEqualTo(2);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+  }
+
+  @Test
+  public void shouldConstructFrom3EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2", 3, "3");
+    assertThat(map.size()).isEqualTo(3);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+  }
+
+  @Test
+  public void shouldConstructFrom4EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2", 3, "3", 4, "4");
+    assertThat(map.size()).isEqualTo(4);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+  }
+
+  @Test
+  public void shouldConstructFrom5EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2", 3, "3", 4, "4", 5, "5");
+    assertThat(map.size()).isEqualTo(5);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+  }
+
+  @Test
+  public void shouldConstructFrom6EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6");
+    assertThat(map.size()).isEqualTo(6);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+  }
+
+  @Test
+  public void shouldConstructFrom7EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6", 7, "7");
+    assertThat(map.size()).isEqualTo(7);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+    assertThat(map.get(7).get()).isEqualTo("7");
+  }
+
+  @Test
+  public void shouldConstructFrom8EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6", 7, "7", 8, "8");
+    assertThat(map.size()).isEqualTo(8);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+    assertThat(map.get(7).get()).isEqualTo("7");
+    assertThat(map.get(8).get()).isEqualTo("8");
+  }
+
+  @Test
+  public void shouldConstructFrom9EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6", 7, "7", 8, "8", 9, "9");
+    assertThat(map.size()).isEqualTo(9);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+    assertThat(map.get(7).get()).isEqualTo("7");
+    assertThat(map.get(8).get()).isEqualTo("8");
+    assertThat(map.get(9).get()).isEqualTo("9");
+  }
+
+  @Test
+  public void shouldConstructFrom10EntriesWithKeyComparator() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(naturalComparator(), 1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6", 7, "7", 8, "8", 9, "9", 10, "10");
+    assertThat(map.size()).isEqualTo(10);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+    assertThat(map.get(7).get()).isEqualTo("7");
+    assertThat(map.get(8).get()).isEqualTo("8");
+    assertThat(map.get(9).get()).isEqualTo("9");
+    assertThat(map.get(10).get()).isEqualTo("10");
+  }
+
+  @Test
+  public void shouldConstructFrom1Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1");
+    assertThat(map.size()).isEqualTo(1);
+    assertThat(map.get(1).get()).isEqualTo("1");
+  }
+
+  @Test
+  public void shouldConstructFrom2Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2");
+    assertThat(map.size()).isEqualTo(2);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+  }
+
+  @Test
+  public void shouldConstructFrom3Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2", 3, "3");
+    assertThat(map.size()).isEqualTo(3);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+  }
+
+  @Test
+  public void shouldConstructFrom4Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2", 3, "3", 4, "4");
+    assertThat(map.size()).isEqualTo(4);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+  }
+
+  @Test
+  public void shouldConstructFrom5Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2", 3, "3", 4, "4", 5, "5");
+    assertThat(map.size()).isEqualTo(5);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+  }
+
+  @Test
+  public void shouldConstructFrom6Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6");
+    assertThat(map.size()).isEqualTo(6);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+  }
+
+  @Test
+  public void shouldConstructFrom7Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6", 7, "7");
+    assertThat(map.size()).isEqualTo(7);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+    assertThat(map.get(7).get()).isEqualTo("7");
+  }
+
+  @Test
+  public void shouldConstructFrom8Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6", 7, "7", 8, "8");
+    assertThat(map.size()).isEqualTo(8);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+    assertThat(map.get(7).get()).isEqualTo("7");
+    assertThat(map.get(8).get()).isEqualTo("8");
+  }
+
+  @Test
+  public void shouldConstructFrom9Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6", 7, "7", 8, "8", 9, "9");
+    assertThat(map.size()).isEqualTo(9);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+    assertThat(map.get(7).get()).isEqualTo("7");
+    assertThat(map.get(8).get()).isEqualTo("8");
+    assertThat(map.get(9).get()).isEqualTo("9");
+  }
+
+  @Test
+  public void shouldConstructFrom10Entries() {
+    final TreeMap<Integer, String> map =
+      TreeMap
+      .of(1, "1", 2, "2", 3, "3", 4, "4", 5, "5", 6, "6", 7, "7", 8, "8", 9, "9", 10, "10");
+    assertThat(map.size()).isEqualTo(10);
+    assertThat(map.get(1).get()).isEqualTo("1");
+    assertThat(map.get(2).get()).isEqualTo("2");
+    assertThat(map.get(3).get()).isEqualTo("3");
+    assertThat(map.get(4).get()).isEqualTo("4");
+    assertThat(map.get(5).get()).isEqualTo("5");
+    assertThat(map.get(6).get()).isEqualTo("6");
+    assertThat(map.get(7).get()).isEqualTo("7");
+    assertThat(map.get(8).get()).isEqualTo("8");
+    assertThat(map.get(9).get()).isEqualTo("9");
+    assertThat(map.get(10).get()).isEqualTo("10");
+  }
+}
