3f364d1933642ecdc6e6ac9437a198b4c1051524
amy.oleary <amy.oleary@autotrader.co.uk>
2017-09-07 01:41:22 +0100

    Add package-info.java file.

---
+++ vavr/src/main/java/io/vavr/control/fluent/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A package for classes that allow fluent chaining of complex functions.
+ * For example {@linkplain io.vavr.control.fluent.Validation} allows for combining validation objects
+ * in a way that will allow parsing of nested objects and complex validation rules.
+ */
+package io.vavr.control.fluent;
