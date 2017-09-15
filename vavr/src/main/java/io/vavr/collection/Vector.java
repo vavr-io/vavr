132e650ef255baa3d956a56b23e4a4b649905293
Daniel Dietrich <cafebab3@gmail.com>
2017-09-16 01:09:15 +0200

    Removed memoization of sortBy() (#2088)

--- vavr/src/main/java/io/vavr/collection/Vector.java
+++ vavr/src/main/java/io/vavr/collection/Vector.java
@@ -1085,12 +1085,7 @@ public final class Vector<T> implements IndexedSeq<T>, Serializable {
 
     @Override
     public <U> Vector<T> sortBy(Comparator<? super U> comparator, Function<? super T, ? extends U> mapper) {
-        Objects.requireNonNull(comparator, "comparator is null");
-        Objects.requireNonNull(mapper, "mapper is null");
-        final Function<? super T, ? extends U> domain = Function1.of(mapper::apply).memoized();
-        return toJavaStream()
-                .sorted((e1, e2) -> comparator.compare(domain.apply(e1), domain.apply(e2)))
-                .collect(collector());
+        return Collections.sortBy(this, comparator, mapper, collector());
     }
 
     @Override
