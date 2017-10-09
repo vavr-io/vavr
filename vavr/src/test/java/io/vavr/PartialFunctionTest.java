aedee8ec9b5d9ead04646908970a99a1f97c24ce
Florian Stefan <florianstefan@hotmail.com>
2017-10-09 10:07:59 +0200

    Implements unlift operator for total functions returning an Option result (#2123)
    * Implements unlift operator for total functions returning an Option result
    
    * Moves serialVersionUID to the top of the interface body
    
    * Add serialVersionUID to anonymous instance
    
    * Uses generic type parameters
    
    * Triggers flickering Java 9 compilation

--- vavr/src/test/java/io/vavr/PartialFunctionTest.java
+++ vavr/src/test/java/io/vavr/PartialFunctionTest.java
@@ -20,8 +20,11 @@
 package io.vavr;
 
 import io.vavr.collection.HashMap;
+import io.vavr.collection.List;
 import io.vavr.control.Option;
 import org.junit.Test;
+
+import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,6 +40,27 @@ public class PartialFunctionTest {
     public void shouldReturnNone() {
         Option<String> oneToOne = HashMap.<Integer, String>empty().lift().apply(1);
         assertThat(oneToOne).isEqualTo(Option.none());
+    }
+
+    @Test
+    public void shouldUnliftTotalFunctionReturningAnOption() {
+        final Predicate<Number> isEven = n -> n.intValue() % 2 == 0;
+        final Function1<Number, Option<String>> totalFunction = n -> isEven.test(n) ? Option.some("even") : Option.none();
+
+        final PartialFunction<Integer, CharSequence> partialFunction = PartialFunction.unlift(totalFunction);
+
+        assertThat(partialFunction.isDefinedAt(1)).isFalse();
+        assertThat(partialFunction.isDefinedAt(2)).isTrue();
+        assertThat(partialFunction.apply(2)).isEqualTo("even");
+    }
+
+    @Test
+    public void shouldCollectSomeValuesAndIgnoreNone() {
+        final List<Integer> evenNumbers = List.range(0, 10)
+          .map(n -> n % 2 == 0 ? Option.some(n) : Option.<Integer>none())
+          .collect(PartialFunction.unlift(Function1.identity()));
+
+        assertThat(evenNumbers).containsExactly(0, 2, 4, 6, 8);
     }
 
 }
