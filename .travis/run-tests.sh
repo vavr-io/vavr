cb02a8e1e72af441d3e4d5c6da4193ee36655513
Conor Restall <conor@restall.io>
2017-11-30 00:19:04 +0000

    change travis to run gradle instead of maven

--- .travis/run-tests.sh
+++ .travis/run-tests.sh
@@ -3,10 +3,10 @@ set -ev
 mvn test -B
 
 # Break CI build on PRs where committed differ from generated sources
-if [ -n "$(git status -su vavr/src-gen)" ]; then
+if [ -n "$(git status -su vavr/src/generated*)" ]; then
   exit 1
 fi
 
-if [ -n "$(git status -su vavr-test/src-gen)" ]; then
+if [ -n "$(git status -su vavr-test/src/generated*)" ]; then
   exit 1
 fi
