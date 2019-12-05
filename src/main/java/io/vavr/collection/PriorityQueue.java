92c6fa01c859c54488e875e438313bf3a898c3e6
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-12-05 20:19:46 +0700

    Provide Comparators.naturalComparator() for PriorityQueue#zipAll

--- src/main/java/io/vavr/collection/PriorityQueue.java
+++ src/main/java/io/vavr/collection/PriorityQueue.java
@@ -626,7 +626,7 @@ public final class PriorityQueue<T> extends io.vavr.collection.AbstractQueue<T, 
     @Override
     public <U> PriorityQueue<Tuple2<T, U>> zipAll(Iterable<? extends U> that, T thisElem, U thatElem) {
         Objects.requireNonNull(that, "that is null");
-        return ofAll(iterator().zipAll(that, thisElem, thatElem));
+        return ofAll(Comparators.naturalComparator(), iterator().zipAll(that, thisElem, thatElem));
     }
 
     @Override
