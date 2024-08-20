ca811ccddfb9b5cef47f8010f42be581c0f23491
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-08-20 09:50:08 +0200

    Copyright update for 2024

--- update-copyright.sh
+++ update-copyright.sh
@@ -5,10 +5,10 @@
 # Prevent sed error 'illegal byte sequence'
 export LC_ALL=C LC_CTYPE=C LANG=C
 
-newYear=`date +'%Y'`
+newYear=$(date +'%Y')
 echo "Updating copyright notice to $newYear"
 
 # Exclude specific directories: -type d \( -path ./.git -o -path ./.ide \) -prune -o
 # Make sed work on Mac: sed -e
 # Prevent making backups: -i ''
-find . -type d \( -path ./.git -o -path ./gradle \) -prune -o -type f -print0 | xargs -0 sed -i '' -e "s/Copyright 2023 Vavr/Copyright $newYear Vavr/"
+find . -type d \( -path ./.git -o -path ./gradle \) -prune -o -type f -print0 | xargs -0 sed -i '' -e "s/Copyright 2024 Vavr/Copyright $newYear Vavr/"
