6a4a981c85f8ae08e66bb47a315a3d063fa91993
Boris van Katwijk <bvkatwijk@gmail.com>
2024-12-30 20:36:10 +0100

    Update nested.sh

--- nested.sh
+++ nested.sh
@@ -5,6 +5,6 @@ find . -name "*Test.java" -type f -exec sed -E -i '' \
     's|// -- ([a-z])([a-zA-Z0-9_]*)(.*)|}\n\n    @Nested\n    @DisplayName("\1\2\3")\n    class \CAPS\1\2 {|' {} +
 
 find . -type f -name '*Test.java' -exec sed -E 's/\bCAPS([a-z])([a-zA-Z]*)/\1 \2/g' {} + | \
-awk '{ gsub(/CAPS([a-z])([a-zA-Z]*)/, "\u\1\2"); print }'
+awk '{ gsub(/CAPS([a-z])([a-zA-Z]*)/, "\\u\1\2"); }'
 
 exit 0;
