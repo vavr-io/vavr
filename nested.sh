4f60de350cdf8da88eff8c92a25ae784b63aa8cf
Boris van Katwijk <bvkatwijk@gmail.com>
2024-12-31 14:41:56 +0100

    Update nested.sh

--- nested.sh
+++ nested.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 find . -name "*Test.java" -type f -exec sed -E -i '' \
     's|// -- ([a-zA-Z])([a-zA-Z0-9_]*)(.*)|}\n\n    @Nested\n    @DisplayName("\1\2\3")\n    class \CAPS\1\2 {|' {} +
 
-find . -type f -name '*Test.java' -exec perl -i -pe 's/\bCAPS([a-z])([a-zA-Z]*)/\u\1\2/g' {} +
+find . -type f -name '*Test.java' -exec perl -i -pe 's/\bCAPS([a-zA-Z])([a-zA-Z]*)/\u\1\2/g' {} +
 
 ./gradlew test
