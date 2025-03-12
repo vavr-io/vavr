e1abf3413aaf94a709172fe7a001dea5d0e51b00
Boris van Katwijk <bvkatwijk@gmail.com>
2025-03-12 12:40:45 +0100

    Remove unused code

--- vavr/src/test/java/io/vavr/OutputTester.java
+++ vavr/src/test/java/io/vavr/OutputTester.java
@@ -18,10 +18,10 @@
  */
 package io.vavr;
 
-import java.io.*;
-import java.nio.charset.Charset;
-
-import static org.assertj.core.api.Assertions.fail;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
 /**
  * Small utility that allows to test code which write to standard error or standard out.
@@ -29,82 +29,6 @@ import static org.assertj.core.api.Assertions.fail;
  * @author Sebastian Zarnekow
  */
 public class OutputTester {
-
-    /**
-     * Encapsulate the standard output and error stream accessors.
-     */
-    private enum Output {
-        OUT {
-            @Override
-            void set(PrintStream stream) {
-                System.setOut(stream);
-            }
-
-            @Override
-            PrintStream get() {
-                return System.out;
-            }
-        },
-        ERR {
-            @Override
-            void set(PrintStream stream) {
-                System.setErr(stream);
-            }
-
-            @Override
-            PrintStream get() {
-                return System.err;
-            }
-        };
-
-        /**
-         * Modifier for the output slot.
-         */
-        abstract void set(PrintStream stream);
-
-        /**
-         * Accessor for the output slot.
-         */
-        abstract PrintStream get();
-
-        /**
-         * Capture the output written to this standard stream and normalize to unix line endings.
-         */
-        String capture(Runnable action) {
-            synchronized (this) {
-                try {
-                    PrintStream orig = get();
-                    try (ByteArrayOutputStream out = new ByteArrayOutputStream(); PrintStream inmemory = new PrintStream(out) {
-                    }) {
-                        set(inmemory);
-                        action.run();
-                        return new String(out.toByteArray(), Charset.defaultCharset()).replace("\r\n", "\n");
-                    } finally {
-                        set(orig);
-                    }
-                } catch (IOException e) {
-                    fail("Unexpected IOException", e);
-                    return "UNREACHABLE";
-                }
-            }
-        }
-
-        /**
-         * Each attempt to write the this standard output will fail with an IOException
-         */
-        void failOnWrite(Runnable action) {
-            synchronized (this) {
-                final PrintStream original = get();
-                try (PrintStream failingPrintStream = failingPrintStream()) {
-                    set(failingPrintStream);
-                    action.run();
-                } finally {
-                    set(original);
-                }
-            }
-        }
-
-    }
 
     private static OutputStream failingOutputStream() {
         return new OutputStream() {
@@ -131,27 +55,6 @@ public class OutputTester {
      */
     public static PrintWriter failingPrintWriter() {
         return new PrintWriter(failingOutputStream());
-    }
-
-    /**
-     * Execute the given runnable in a context, where each attempt to
-     * write to stderr will fail with an {@link IOException}
-     *
-     * @param runnable the runnable to be executed.
-     */
-    public static void withFailingErrOut(Runnable runnable) {
-        Output.ERR.failOnWrite(runnable);
-    }
-
-    /**
-     * Execute the given runnable in a context, where each attempt to
-     * write to stdout will fail with an {@link IOException}
-     *
-     * @param runnable the runnable to be executed.
-     */
-
-    public static void withFailingStdOut(Runnable runnable) {
-        Output.OUT.failOnWrite(runnable);
     }
 
 }
