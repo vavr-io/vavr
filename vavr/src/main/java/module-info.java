1c80e648aa86f2ccddd36fe4db85feba96e7c815
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-12-14 22:34:38 +0400

    Upgrade min JDK requirement to 21

---
+++ vavr/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.vavr {
+    exports io.vavr;
+    exports io.vavr.collection;
+    exports io.vavr.control;
+    exports io.vavr.concurrent;
+    requires io.vavr.match;
+    requires static org.jspecify;
+}
