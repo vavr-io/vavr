a13875fef7ef30ea1bc2df0f80d13c79162d30f2
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-01-10 12:24:24 +0100

    Leverage spotless to manage license headers

--- vavr/src-gen/main/java/io/vavr/Tuple5.java
+++ vavr/src-gen/main/java/io/vavr/Tuple5.java
@@ -1,7 +1,6 @@
-/*  __    __  __  __    __  ___
- * \  \  /  /    \  \  /  /  __/
- *  \  \/  /  /\  \  \/  /  /
- *   \____/__/  \__\____/__/
+/* ____  ______________  ________________________  __________
+ * \   \/   /      \   \/   /   __/   /      \   \/   /      \
+ *  \______/___/\___\______/___/_____/___/\___\______/___/\___\
  *
  * Copyright 2014-2026 Vavr, https://vavr.io
  *
@@ -9,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
