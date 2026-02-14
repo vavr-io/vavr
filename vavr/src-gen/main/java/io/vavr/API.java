02055fd6371372b19e05d0cc48db1415bfb74fd2
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-02-14 20:28:20 +0100

    Merge genMapAliases + genSortedMapAliases into a single genMapAliases (#3237)

--- vavr/src-gen/main/java/io/vavr/API.java
+++ vavr/src-gen/main/java/io/vavr/API.java
@@ -1978,6 +1978,8 @@ public final class API {
     public static <K, V> Map<K, V> LinkedMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10) {
         return LinkedHashMap.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8, k9, v9, k10, v10);
     }
+    // -- SortedMap
+
     /**
      * Alias for {@link TreeMap#empty()}
      *
